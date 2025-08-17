81cca65ea41138ce39c06fc5e283367fefcc109a
Alex Musayev <alex.musayev@gmail.com>
2025-08-17 17:37:59 +0200

    Improve test configuration and FactoryBot usage

--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -2,32 +2,30 @@ require "test_helper"
 
 class SessionTest < ActiveSupport::TestCase
   test "should be valid with user" do
-    user = users(:one)
-    session = Session.new(user: user, ip_address: "127.0.0.1", user_agent: "Test Browser")
+    session = build(:session)
     assert session.valid?
   end
 
   test "should require user" do
-    session = Session.new(ip_address: "127.0.0.1", user_agent: "Test Browser")
+    session = build(:session, user: nil)
     assert_not session.valid?
     assert session.errors.of_kind?(:user, :blank)
   end
 
   test "should belong to user" do
-    user = users(:one)
-    session = Session.create!(user: user, ip_address: "127.0.0.1", user_agent: "Test Browser")
+    user = create(:user)
+    session = create(:session, user: user)
     assert_equal user, session.user
   end
 
   test "should allow optional ip_address and user_agent" do
-    user = users(:one)
-    session = Session.new(user: user)
+    session = build(:session, ip_address: nil, user_agent: nil)
     assert session.valid?
   end
 
   test "should be destroyed when user is destroyed" do
-    user = users(:one)
-    session = Session.create!(user: user, ip_address: "127.0.0.1", user_agent: "Test Browser")
+    user = create(:user)
+    session = create(:session, user: user)
 
     assert_difference "Session.count", -1 do
       user.destroy
