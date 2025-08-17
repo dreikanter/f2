eb97b45b20b59d3d664669c6db8ac5cbca0b3765
Alex Musayev <alex.musayev@gmail.com>
2025-08-17 17:40:31 +0200

    Clean up FeedRefreshJob test

--- test/jobs/feed_refresh_job_test.rb
+++ test/jobs/feed_refresh_job_test.rb
@@ -1,36 +1,9 @@
 require "test_helper"
 
 class FeedRefreshJobTest < ActiveJob::TestCase
-  test "refreshes feed by id" do
-    feed = create(:feed)
-
-    # Capture log output to verify the job ran
-    log_output = capture_log do
-      FeedRefreshJob.perform_now(feed.id)
-    end
-
-    assert_includes log_output, "Refreshing feed: #{feed.name}"
-    assert_includes log_output, feed.url
-  end
-
   test "handles missing feed gracefully" do
-    # Should not raise an error, just exit early
     assert_nothing_raised do
-      FeedRefreshJob.perform_now(999999)
+      FeedRefreshJob.perform_now(-1)
     end
-  end
-
-  private
-
-  def capture_log
-    log_stream = StringIO.new
-    old_logger = Rails.logger
-    Rails.logger = Logger.new(log_stream)
-
-    yield
-
-    log_stream.string
-  ensure
-    Rails.logger = old_logger
   end
 end
