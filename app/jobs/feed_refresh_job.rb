b5fae7fb168c48bdd963eb22ca8c55e511995c90
Alex Musayev <alex.musayev@gmail.com>
2025-08-17 16:27:54 +0200

    Handle missing feeds gracefully in refresh job

--- app/jobs/feed_refresh_job.rb
+++ app/jobs/feed_refresh_job.rb
@@ -2,11 +2,12 @@ class FeedRefreshJob < ApplicationJob
   queue_as :default
 
   def perform(feed_id)
-    feed = Feed.find(feed_id)
-    
+    feed = Feed.find_by(id: feed_id)
+    return unless feed
+
     # TODO: Implement actual feed fetching logic
     Rails.logger.info "Refreshing feed: #{feed.name} (#{feed.url})"
-    
+
     # Future implementation will:
     # 1. Use feed.loader to fetch data from feed.url
     # 2. Use feed.processor to parse the data format
--- test/jobs/feed_refresh_job_test.rb
+++ test/jobs/feed_refresh_job_test.rb
@@ -3,18 +3,19 @@ require "test_helper"
 class FeedRefreshJobTest < ActiveJob::TestCase
   test "refreshes feed by id" do
     feed = create(:feed)
-    
+
     # Capture log output to verify the job ran
     log_output = capture_log do
       FeedRefreshJob.perform_now(feed.id)
     end
-    
+
     assert_includes log_output, "Refreshing feed: #{feed.name}"
     assert_includes log_output, feed.url
   end
 
   test "handles missing feed gracefully" do
-    assert_raises(ActiveRecord::RecordNotFound) do
+    # Should not raise an error, just exit early
+    assert_nothing_raised do
       FeedRefreshJob.perform_now(999999)
     end
   end
@@ -25,9 +26,9 @@ class FeedRefreshJobTest < ActiveJob::TestCase
     log_stream = StringIO.new
     old_logger = Rails.logger
     Rails.logger = Logger.new(log_stream)
-    
+
     yield
-    
+
     log_stream.string
   ensure
     Rails.logger = old_logger
