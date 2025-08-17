2269904eb4c09c9979d7af7aaf5d59ed5ba71185
Alex Musayev <alex.musayev@gmail.com>
2025-08-17 16:54:04 +0200

    Refactor feed scheduler job for better readability

--- app/jobs/feed_scheduler_job.rb
+++ app/jobs/feed_scheduler_job.rb
@@ -3,25 +3,43 @@ class FeedSchedulerJob < ApplicationJob
 
   def perform
     Feed.due.find_each do |feed|
-      schedule = feed.feed_schedule
-
-      if schedule
-        updated = FeedSchedule
-          .where(id: schedule.id, next_run_at: schedule.next_run_at)
-          .update_all(next_run_at: schedule.calculate_next_run_at, last_run_at: Time.current)
-
-        if updated == 1
-          FeedRefreshJob.perform_later(feed.id)
-        end
-      else
-        # Create initial schedule and queue job
-        FeedSchedule.create!(
-          feed: feed,
-          next_run_at: Time.current,
-          last_run_at: Time.current
-        )
-        FeedRefreshJob.perform_later(feed.id)
-      end
+      next unless schedule_feed_refresh?(feed)
+      FeedRefreshJob.perform_later(feed.id)
     end
+  end
+
+  private
+
+  def schedule_feed_refresh?(feed)
+    schedule = feed.feed_schedule
+
+    if schedule
+      updated_records_count = update_existing_schedule(schedule)
+      updated_records_count == 1
+    else
+      create_initial_schedule(feed)
+      true
+    end
+  end
+
+  def update_existing_schedule(schedule)
+    FeedSchedule
+      .where(id: schedule.id, next_run_at: schedule.next_run_at)
+      .update_all(
+        next_run_at: schedule.calculate_next_run_at,
+        last_run_at: current_time
+      )
+  end
+
+  def create_initial_schedule(feed)
+    FeedSchedule.create!(
+      feed: feed,
+      next_run_at: current_time,
+      last_run_at: current_time
+    )
+  end
+
+  def current_time
+    @current_time ||= Time.current
   end
 end
