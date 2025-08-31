6daa6575e8689796fcd0b7b7eb4be58eea15a19c
Alex Musayev <alex.musayev@gmail.com>
2025-08-31 03:17:44 +0200

    Clean up the tests

--- test/models/application_record_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ApplicationRecordTest < ActiveSupport::TestCase
-  test "should inherit from ActiveRecord::Base" do
-    assert ApplicationRecord < ActiveRecord::Base
-  end
-end
