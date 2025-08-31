0acb130575fa783a78becf50e57d59e8e8d440cc
Alex Musayev <alex.musayev@gmail.com>
2025-08-31 12:34:26 +0200

    Clean up

--- app/jobs/feed_refresh_job.rb
+++ app/jobs/feed_refresh_job.rb
@@ -2,9 +2,6 @@ class FeedRefreshJob < ApplicationJob
   queue_as :default
 
   def perform(feed_id)
-    feed = Feed.find_by(id: feed_id)
-    nil unless feed
-
     # TODO: Implement actual feed fetching logic
     # 1. Use feed.loader to fetch data from feed.url
     # 2. Use feed.processor to parse the data format
