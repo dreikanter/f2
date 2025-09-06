df49ded9791e8840c5967d6de3b4c5a7f26afe81
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 13:52:55 +0200

    Update routes for separated email and password update controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resource :profile, only: [:show, :update]
+  resource :profile, only: [:show]
+  resources :email_updates, only: [:update]
+  resources :password_updates, only: [:update]
   resources :email_confirmations, only: [:show], param: :token
   resource :dashboard, only: :show
   resource :session
