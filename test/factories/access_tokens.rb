ba1915e5a73747a7ee85ec091796074db8322765
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 21:29:17 +0200

    Update test factories for status enum and user tokens

--- test/factories/access_tokens.rb
+++ test/factories/access_tokens.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :access_token do
     association :user
     sequence(:name) { |n| "Token #{n}" }
-    is_active { true }
+    status { :active }
     last_used_at { nil }
 
     transient do
@@ -11,14 +11,15 @@ FactoryBot.define do
 
     after(:build) do |access_token, evaluator|
       if evaluator.with_token
-        access_token.generate_token
+        # Simulate user-provided Freefeed token
+        access_token.token = "freefeed_token_#{SecureRandom.hex(16)}"
       elsif access_token.token_digest.blank?
         access_token.token_digest = BCrypt::Password.create("dummy_token_#{SecureRandom.hex(8)}")
       end
     end
 
     trait :inactive do
-      is_active { false }
+      status { :inactive }
     end
 
     trait :recently_used do
