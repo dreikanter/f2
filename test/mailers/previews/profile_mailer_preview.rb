af89470607d12aa208943613f12d8a7dc06ef60f
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 14:38:11 +0200

    Fix mailer preview

--- test/mailers/previews/profile_mailer_preview.rb
+++ test/mailers/previews/profile_mailer_preview.rb
@@ -2,6 +2,6 @@
 class ProfileMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/profile_mailer/email_change_confirmation
   def email_change_confirmation
-    ProfileMailer.email_change_confirmation
+    ProfileMailer.email_change_confirmation(User.new(email_address: "old_email@example.com"), "new_email@example.com")
   end
 end
--- test/mailers/profile_mailer_test.rb
+++ test/mailers/profile_mailer_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ProfileMailerTest < ActionMailer::TestCase
   test "email_change_confirmation" do
-    mail = ProfileMailer.email_change_confirmation
+    mail = ProfileMailer.email_change_confirmation(build(:user), "new_email@example.com")
     assert_equal "Email change confirmation", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
