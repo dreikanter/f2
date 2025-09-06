80da6aa3b6d909fcb7af48807d5ebfcc0a5cfeda
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 20:53:06 +0200

    Add TokenValidationService for API authentication

---
+++ app/services/token_validation_service.rb
@@ -0,0 +1,20 @@
+class TokenValidationService
+  def self.validate_token(token_string)
+    return false if token_string.blank?
+
+    access_token = find_token_by_value(token_string)
+    return false unless access_token&.is_active?
+
+    access_token.touch_last_used!
+    access_token.user
+  end
+
+  private
+
+  def self.find_token_by_value(token_string)
+    AccessToken.active.find_each do |token|
+      return token if token.authenticate(token_string)
+    end
+    nil
+  end
+end
