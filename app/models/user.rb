e741d860c0b39d01f2ed3c6292f9fd3f1e175132
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 12:12:33 +0200

    Add permissions association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :feeds, dependent: :destroy
+  has_many :permissions, dependent: :destroy
 
   validates :email_address, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
