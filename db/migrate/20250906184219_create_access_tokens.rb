a643d04dd21ea3bae44e15d993179a06ea3d0700
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 20:52:51 +0200

    Add access_tokens database migration

---
+++ db/migrate/20250906184219_create_access_tokens.rb
@@ -0,0 +1,15 @@
+class CreateAccessTokens < ActiveRecord::Migration[8.0]
+  def change
+    create_table :access_tokens do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.string :token_digest, null: false
+      t.boolean :is_active, null: false, default: true
+      t.datetime :last_used_at
+
+      t.timestamps
+    end
+
+    add_index :access_tokens, [:user_id, :name], unique: true
+  end
+end
