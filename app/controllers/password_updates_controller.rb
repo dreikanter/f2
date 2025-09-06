2b88b3b8b4c274c646a5d92cd02eadf72a31c84e
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 13:52:41 +0200

    Add PasswordUpdatesController for password changes

---
+++ app/controllers/password_updates_controller.rb
@@ -0,0 +1,25 @@
+class PasswordUpdatesController < ApplicationController
+  def update
+    @user = Current.user
+
+    if current_password_correct?
+      if @user.update(password_params)
+        redirect_to profile_path, notice: "Password updated successfully."
+      else
+        redirect_to profile_path, alert: @user.errors.full_messages.join(", ")
+      end
+    else
+      redirect_to profile_path, alert: "Current password is incorrect."
+    end
+  end
+
+  private
+
+  def current_password_correct?
+    @user.authenticate(params[:user][:current_password])
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+end
