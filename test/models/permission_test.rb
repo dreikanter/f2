c34a98b60dbdba0fdf5a7aa15ce6e439cd3eddd1
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 12:12:55 +0200

    Add comprehensive test coverage for permissions

---
+++ test/models/permission_test.rb
@@ -0,0 +1,56 @@
+require "test_helper"
+
+class PermissionTest < ActiveSupport::TestCase
+  def permission
+    @permission ||= build(:permission)
+  end
+
+  def user
+    @user ||= create(:user)
+  end
+
+  test "should be valid with user and valid name" do
+    assert permission.valid?
+  end
+
+  test "should belong to user" do
+    permission = create(:permission, user: user)
+    assert_equal user, permission.user
+  end
+
+  test "should require name" do
+    permission = build(:permission, name: nil)
+    assert_not permission.valid?
+    assert permission.errors.of_kind?(:name, :blank)
+  end
+
+  test "should require user" do
+    permission = build(:permission, user: nil)
+    assert_not permission.valid?
+    assert permission.errors.of_kind?(:user, :blank)
+  end
+
+  test "should validate name is in available permissions" do
+    permission = build(:permission, name: "invalid_permission")
+    assert_not permission.valid?
+    assert permission.errors.of_kind?(:name, :inclusion)
+  end
+
+  test "should not allow duplicate permission for same user" do
+    existing_permission = create(:permission, user: user, name: "admin")
+    duplicate_permission = build(:permission, user: user, name: "admin")
+
+    assert_not duplicate_permission.valid?
+    assert duplicate_permission.errors.of_kind?(:user_id, :taken)
+  end
+
+  test "should allow same permission for different users" do
+    user1 = create(:user)
+    user2 = create(:user)
+
+    permission1 = create(:permission, user: user1, name: "admin")
+    permission2 = build(:permission, user: user2, name: "admin")
+
+    assert permission2.valid?
+  end
+end
