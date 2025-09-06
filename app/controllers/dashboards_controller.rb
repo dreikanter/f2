758eb163bf40f2929771c78bf0ebc6df5ce576f1
Alex Musayev <alex.musayev@gmail.com>
2025-09-06 20:53:39 +0200

    Add access token info to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,3 +1,8 @@
 class DashboardsController < ApplicationController
   before_action :require_authentication
+
+  def show
+    @user = Current.user
+    @access_tokens_count = @user.access_tokens.active.count
+  end
 end
