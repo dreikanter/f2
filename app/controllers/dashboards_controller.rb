2e3452b850671cd7f9672e574e6d7c19b7e8a1dc
Alex Musayev <alex.musayev@gmail.com>
2025-09-07 17:06:25 +0200

    Clean up

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,3 @@
 class DashboardsController < ApplicationController
   before_action :require_authentication
-
-  def show
-    @user = Current.user
-    @access_tokens_count = @user.access_tokens.active.count
-  end
 end
