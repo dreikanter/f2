767fd53d2956b1f8422895f82c7f012ab6c82ba7
Alex Musayev <alex.musayev@gmail.com>
2025-09-08 22:58:40 +0200

    Add Stimulus polling controller for token validation

---
+++ app/javascript/controllers/polling_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { tokenId: String }
+  
+  connect() {
+    if (this.hasTokenIdValue) {
+      this.startPolling();
+    }
+  }
+  
+  startPolling() {
+    this.interval = setInterval(() => {
+      fetch(`/access_tokens/${this.tokenIdValue}/token_validations/${this.tokenIdValue}/status`, {
+        headers: { 
+          "Accept": "text/vnd.turbo-stream.html",
+          "X-Requested-With": "XMLHttpRequest"
+        }
+      })
+      .then(response => response.text())
+      .then(html => {
+        // Process the Turbo Stream response
+        Turbo.renderStreamMessage(html);
+        
+        // Check if job is complete by looking for final states
+        if (html.includes('data-status="active"') || 
+            html.includes('data-status="inactive"')) {
+          clearInterval(this.interval);
+        }
+      })
+      .catch(error => {
+        console.error('Polling error:', error);
+        clearInterval(this.interval);
+      });
+    }, 2000);
+  }
+  
+  disconnect() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+}
