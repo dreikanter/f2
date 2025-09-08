d5e2b15e0891a36429e8a9527234314ebe8f6c8a
Alex Musayev <alex.musayev@gmail.com>
2025-09-08 23:17:08 +0200

    Revert `ApplicationCable` deletion

---
+++ test/channels/application_cable/connection_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
+  test "connects with valid session" do
+    user = create(:user)
+    session = user.sessions.create!(user_agent: "test", ip_address: "127.0.0.1")
+
+    cookies.signed[:session_id] = session.id
+
+    connect
+
+    assert_equal user, connection.current_user
+  end
+
+  test "rejects connection without valid session" do
+    assert_reject_connection { connect }
+  end
+end
