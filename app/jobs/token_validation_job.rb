e73b47364e4526371084abe982ba62549a6b4e78
Alex Musayev <alex.musayev@gmail.com>
2025-09-11 00:03:41 +0300

    Add FreeFeed API client (#28)
    * Add FreefeedClient for API token validation
    
    * Add comprehensive tests for FreefeedClient
    
    * Update TokenValidationJob to use FreefeedClient
    
    * Update TokenValidationJob tests for FreefeedClient
    
    * Add comment explaining HTTP client abstraction

--- app/jobs/token_validation_job.rb
+++ app/jobs/token_validation_job.rb
@@ -5,13 +5,10 @@ class TokenValidationJob < ApplicationJob
     return unless access_token.token_value.present?
 
     begin
-      response = validate_freefeed_token(access_token.token_value)
-
-      if response[:success] && response[:username]
-        access_token.update!(status: :active, owner: response[:username])
-      else
-        access_token.inactive!
-      end
+      user_info = freefeed_client(access_token.token_value).whoami
+      access_token.update!(status: :active, owner: user_info[:username])
+    rescue FreefeedClient::UnauthorizedError
+      access_token.inactive!
     rescue => e
       access_token.inactive!
     end
@@ -19,51 +16,8 @@ class TokenValidationJob < ApplicationJob
 
   private
 
-  def validate_freefeed_token(token)
-    response = make_api_request(token)
-    parse_api_response(response)
-  rescue JSON::ParserError
-    { success: false, error: "Invalid JSON response" }
-  rescue HttpClient::TooManyRedirectsError => e
-    { success: false, error: "Too many redirects" }
-  rescue HttpClient::Error => e
-    { success: false, error: e.message }
-  rescue => e
-    { success: false, error: e.message }
-  end
-
-  def make_api_request(token)
-    http_client.get(
-      "#{freefeed_host}/v4/users/whoami",
-      headers: {
-        "Authorization" => "Bearer #{token}",
-        "Accept" => "application/json",
-        "User-Agent" => "FreeFeed-Token-Validator"
-      }
-    )
-  end
-
-  def parse_api_response(response)
-    if response.success?
-      parse_success_response(response.body)
-    else
-      { success: false, error: "HTTP #{response.status}: #{response.body}" }
-    end
-  end
-
-  def parse_success_response(body)
-    data = JSON.parse(body)
-    username = data.dig("users", "username")
-
-    if username
-      { success: true, username: username }
-    else
-      { success: false, error: "Invalid response format" }
-    end
-  end
-
-  def http_client
-    @http_client ||= HttpClient::FaradayAdapter.new
+  def freefeed_client(token)
+    FreefeedClient.new(host: freefeed_host, token: token)
   end
 
   def freefeed_host
