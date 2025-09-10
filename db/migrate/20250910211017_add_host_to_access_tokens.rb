2ce78e015bee70297b9955dfb1169f80f51382f6
Alex Musayev <alex.musayev@gmail.com>
2025-09-10 23:25:42 +0200

    Remove default value from host migration

--- db/migrate/20250910211017_add_host_to_access_tokens.rb
+++ db/migrate/20250910211017_add_host_to_access_tokens.rb
@@ -1,5 +1,5 @@
 class AddHostToAccessTokens < ActiveRecord::Migration[8.0]
   def change
-    add_column :access_tokens, :host, :string, null: false, default: "https://freefeed.net"
+    add_column :access_tokens, :host, :string, null: false
   end
 end
