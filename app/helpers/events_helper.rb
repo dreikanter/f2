9cf095d6a8bcfd08032393463484bd57a2d5d291
Alex Musayev <alex.musayev@gmail.com>
2025-09-12 13:14:27 +0200

    Add full word level badge for event show page

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -11,6 +11,11 @@ module EventsHelper
     content_tag(:span, badge[:char], class: "#{badge[:class]} font-monospace", title: level.humanize)
   end
 
+  def level_badge_full(level)
+    badge = LEVEL_BADGES.fetch(level.to_s, LEVEL_BADGES["debug"])
+    content_tag(:span, level.humanize, class: badge[:class])
+  end
+
   def compact_time_ago(time)
     seconds = Time.current - time
     
