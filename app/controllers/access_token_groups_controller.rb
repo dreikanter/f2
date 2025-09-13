57f6af2b1785fa492859330a3ea1ffc8d6a3da83
Alex Musayev <alex.musayev@gmail.com>
2025-09-14 00:19:58 +0200

    Extract and memoize FreefeedClient in groups controller

--- app/controllers/access_token_groups_controller.rb
+++ app/controllers/access_token_groups_controller.rb
@@ -2,14 +2,8 @@ class AccessTokenGroupsController < ApplicationController
   before_action :load_access_token
 
   def index
-    client = FreefeedClient.new(
-      host: @access_token.host,
-      token: @access_token.token_value
-    )
-
-    groups = client.managed_groups
     render json: {
-      groups: groups.map { |group|
+      groups: managed_groups.map { |group|
         {
           id: group[:id],
           username: group[:username],
@@ -27,5 +21,16 @@ class AccessTokenGroupsController < ApplicationController
 
   def load_access_token
     @access_token = Current.user.access_tokens.find(params[:access_token_id])
+  end
+
+  def freefeed_client
+    @freefeed_client ||= FreefeedClient.new(
+      host: @access_token.host,
+      token: @access_token.token_value
+    )
+  end
+
+  def managed_groups
+    @managed_groups ||= freefeed_client.managed_groups
   end
 end
