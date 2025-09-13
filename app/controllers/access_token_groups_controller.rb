d801ada0bf7c9e5e78642d2c6aa3d39afcfedd05
Alex Musayev <alex.musayev@gmail.com>
2025-09-14 00:27:53 +0200

    Add Turbo Stream support to groups controller

--- app/controllers/access_token_groups_controller.rb
+++ app/controllers/access_token_groups_controller.rb
@@ -2,19 +2,27 @@ class AccessTokenGroupsController < ApplicationController
   before_action :load_access_token
 
   def index
-    render json: {
-      groups: managed_groups.map { |group|
-        {
-          id: group[:id],
-          username: group[:username],
-          screen_name: group[:screen_name],
-          is_private: group[:is_private],
-          is_restricted: group[:is_restricted]
+    respond_to do |format|
+      format.turbo_stream
+      format.json do
+        render json: {
+          groups: managed_groups.map { |group|
+            {
+              id: group[:id],
+              username: group[:username],
+              screen_name: group[:screen_name],
+              is_private: group[:is_private],
+              is_restricted: group[:is_restricted]
+            }
+          }
         }
-      }
-    }
+      end
+    end
   rescue FreefeedClient::Error => e
-    render json: { error: e.message }, status: :unprocessable_entity
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("group-select-wrapper", partial: "access_token_groups/error", locals: { error: e.message }) }
+      format.json { render json: { error: e.message }, status: :unprocessable_entity }
+    end
   end
 
   private
