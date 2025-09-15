c539a3d066d7de08b08e7149ada4d1f1425a6a33
Alex Musayev <alex.musayev@gmail.com>
2025-09-15 21:33:33 +0200

    Reduce the number of test record creations

--- test/helpers/feed_helper_test.rb
+++ test/helpers/feed_helper_test.rb
@@ -2,16 +2,14 @@ require "test_helper"
 
 class FeedHelperTest < ActionView::TestCase
   test "feed_missing_enablement_parts returns both missing parts" do
-    feed = create(:feed, :without_access_token)
-
+    feed = build(:feed, :without_access_token)
     result = feed_missing_enablement_parts(feed)
 
     assert_equal ["active access token", "target group"], result
   end
 
   test "feed_missing_enablement_parts returns missing access token only" do
-    feed = create(:feed, :without_access_token, target_group: "test_group")
-
+    feed = build(:feed, :without_access_token, target_group: "test_group")
     result = feed_missing_enablement_parts(feed)
 
     assert_equal ["active access token"], result
@@ -19,8 +17,7 @@ class FeedHelperTest < ActionView::TestCase
 
   test "feed_missing_enablement_parts returns missing target group only" do
     access_token = create(:access_token, :active)
-    feed = create(:feed, access_token: access_token, target_group: nil)
-
+    feed = build(:feed, access_token: access_token, target_group: nil)
     result = feed_missing_enablement_parts(feed)
 
     assert_equal ["target group"], result
@@ -28,8 +25,7 @@ class FeedHelperTest < ActionView::TestCase
 
   test "feed_missing_enablement_parts returns missing access token when inactive" do
     access_token = create(:access_token, :inactive)
-    feed = create(:feed, access_token: access_token, target_group: "test_group")
-
+    feed = build(:feed, access_token: access_token, target_group: "test_group")
     result = feed_missing_enablement_parts(feed)
 
     assert_equal ["active access token"], result
@@ -37,8 +33,7 @@ class FeedHelperTest < ActionView::TestCase
 
   test "feed_missing_enablement_parts returns empty array when all requirements met" do
     access_token = create(:access_token, :active)
-    feed = create(:feed, access_token: access_token, target_group: "test_group")
-
+    feed = build(:feed, access_token: access_token, target_group: "test_group")
     result = feed_missing_enablement_parts(feed)
 
     assert_equal [], result
