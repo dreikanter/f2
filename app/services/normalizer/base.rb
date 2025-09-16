7be309d94aa7e3f0fb069d90548ebf22afd094c5
Alex Musayev <alex.musayev@gmail.com>
2025-09-16 20:55:05 +0200

    Refactor Normalizer::Base to build Post objects

--- app/services/normalizer/base.rb
+++ app/services/normalizer/base.rb
@@ -1,16 +1,26 @@
 module Normalizer
   class Base
-    def initialize(feed, processed_items)
-      @feed = feed
-      @processed_items = processed_items
+    def initialize(feed_entry)
+      @feed_entry = feed_entry
     end
 
     def normalize
-      raise NotImplementedError, "Subclasses must implement #normalize method"
+      post = build_post
+      post.validation_errors = validate_post(post)
+      post.status = post.validation_errors.empty? ? :enqueued : :rejected
+      post
     end
 
     private
 
-    attr_reader :feed, :processed_items
+    attr_reader :feed_entry
+
+    def build_post
+      raise NotImplementedError, "Subclasses must implement #build_post method"
+    end
+
+    def validate_post(post)
+      raise NotImplementedError, "Subclasses must implement #validate_post method"
+    end
   end
 end
