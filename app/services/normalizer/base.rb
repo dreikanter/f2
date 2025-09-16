a6ec938a1de34300ec814b2d569f29e8843714c3
Alex Musayev <alex.musayev@gmail.com>
2025-09-16 21:10:28 +0200

    Add YARD documentation to Normalizer::Base

--- app/services/normalizer/base.rb
+++ app/services/normalizer/base.rb
@@ -1,9 +1,13 @@
 module Normalizer
+  # Base class for feed entry normalizers
   class Base
+    # @param feed_entry [FeedEntry] the feed entry to normalize
     def initialize(feed_entry)
       @feed_entry = feed_entry
     end
 
+    # Normalizes feed entry into a Post with validation
+    # @return [Post] post with status set based on validation
     def normalize
       post = build_post
       post.validation_errors = validate_post(post)
@@ -15,10 +19,17 @@ module Normalizer
 
     attr_reader :feed_entry
 
+    # Builds Post from feed entry data
+    # @return [Post] new post instance
+    # @abstract Subclasses must implement this method
     def build_post
       raise NotImplementedError, "Subclasses must implement #build_post method"
     end
 
+    # Validates post data
+    # @param post [Post] the post to validate
+    # @return [Array<String>] validation error codes
+    # @abstract Subclasses must implement this method
     def validate_post(post)
       raise NotImplementedError, "Subclasses must implement #validate_post method"
     end
