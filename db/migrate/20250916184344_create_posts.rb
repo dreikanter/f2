9847656b928791601da6acccb44f199ac685e74b
Alex Musayev <alex.musayev@gmail.com>
2025-09-16 20:54:41 +0200

    Add Post model migration

---
+++ db/migrate/20250916184344_create_posts.rb
@@ -0,0 +1,21 @@
+class CreatePosts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :posts do |t|
+      t.references :feed, null: false, foreign_key: true
+      t.references :feed_entry, null: true, foreign_key: true
+      t.string :uid, null: false
+      t.integer :status, default: 0, null: false
+      t.datetime :published_at, null: false
+      t.string :link, null: false
+      t.text :text, default: "", null: false
+      t.text :attachment_urls, default: [], null: false, array: true
+      t.text :comments, default: [], null: false, array: true
+      t.string :freefeed_post_id
+      t.text :validation_errors, default: [], null: false, array: true
+
+      t.timestamps
+    end
+
+    add_index :posts, [:feed_id, :uid], unique: true
+  end
+end
