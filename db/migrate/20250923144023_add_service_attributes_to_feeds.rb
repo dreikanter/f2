a0f01222c5b84e714723ae212d608a0a8fb0269f
Alex Musayev <alex.musayev@gmail.com>
2025-09-23 22:17:09 +0200

    Delete add_service_attributes_to_feeds migration

--- db/migrate/20250923144023_add_service_attributes_to_feeds.rb
+++
@@ -1,7 +0,0 @@
-class AddServiceAttributesToFeeds < ActiveRecord::Migration[8.1]
-  def change
-    add_column :feeds, :loader, :string, null: false, default: "http"
-    add_column :feeds, :processor, :string, null: false, default: "rss"
-    add_column :feeds, :normalizer, :string, null: false, default: "rss"
-  end
-end
