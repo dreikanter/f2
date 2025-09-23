807c48664096a6f552569bd746121741ce699e73
Alex Musayev <alex.musayev@gmail.com>
2025-09-23 12:41:57 +0200

    Update test factories for feed profiles

---
+++ test/factories/feed_profiles.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :feed_profile do
+    association :user
+    sequence(:name) { |n| "profile-#{n}-#{SecureRandom.uuid[0..7]}" }
+    loader { "http" }
+    processor { "rss" }
+    normalizer { "rss" }
+
+    trait :rss do
+      sequence(:name) { |n| "rss-#{n}-#{SecureRandom.uuid[0..7]}" }
+      loader { "http" }
+      processor { "rss" }
+      normalizer { "rss" }
+    end
+  end
+end
--- test/factories/feeds.rb
+++ test/factories/feeds.rb
@@ -1,12 +1,9 @@
 FactoryBot.define do
   factory :feed do
     association :user
-    sequence(:name) { |n| "sample-feed-#{n}" }
+    sequence(:name) { |n| "sample-feed-#{n}-#{SecureRandom.uuid[0..7]}" }
     url { "https://example.com/feed.xml" }
     cron_expression { "0 */6 * * *" }
-    loader { "http_loader" }
-    processor { "rss_processor" }
-    normalizer { "rss_normalizer" }
     state { :disabled }
     description { "" }
     import_after { nil }
@@ -15,6 +12,9 @@ FactoryBot.define do
     after(:build) do |feed|
       if feed.user && feed.access_token.nil?
         feed.access_token = create(:access_token, :active, user: feed.user)
+      end
+      if feed.user && feed.feed_profile.nil?
+        feed.feed_profile = create(:feed_profile, :rss, user: feed.user)
       end
     end
 
@@ -32,6 +32,10 @@ FactoryBot.define do
       access_token { nil }
       target_group { nil }
       after(:build) { |feed| feed.access_token = nil }
+    end
+
+    trait :without_feed_profile do
+      after(:build) { |feed| feed.feed_profile = nil }
     end
   end
 end
