3e9542f066010ec0d49a7c962f60316d0305abfd
Alex Musayev <alex.musayev@gmail.com>
2025-09-25 17:22:07 +0200

    Improve page_header tests to use assert_equal with complete HTML

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,31 +3,26 @@ require "test_helper"
 class ApplicationHelperTest < ActionView::TestCase
   test "page_header without block renders title with simple layout" do
     result = page_header("Test Title")
+    expected = '<div class="mb-4"><h1>Test Title</h1></div>'
 
-    assert_includes result, '<div class="mb-4">'
-    assert_includes result, '<h1>Test Title</h1>'
-    assert_not_includes result, "d-flex"
+    assert_equal expected, result
   end
 
   test "page_header with block renders title and content with flex layout" do
     result = page_header("Test Title") do
       content_tag(:a, "Link", href: "/test", class: "btn btn-primary")
     end
+    expected = '<div class="d-flex justify-content-between align-items-center mb-4"><h1>Test Title</h1><a href="/test" class="btn btn-primary">Link</a></div>'
 
-    assert_includes result, '<div class="d-flex justify-content-between align-items-center mb-4">'
-    assert_includes result, '<h1>Test Title</h1>'
-    assert_includes result, 'href="/test"'
-    assert_includes result, 'class="btn btn-primary"'
-    assert_includes result, '>Link</a>'
+    assert_equal expected, result
   end
 
   test "page_header with text block content" do
     result = page_header("Settings") do
       "Some content"
     end
+    expected = '<div class="d-flex justify-content-between align-items-center mb-4"><h1>Settings</h1>Some content</div>'
 
-    assert_includes result, '<h1>Settings</h1>'
-    assert_includes result, "Some content"
-    assert_includes result, "d-flex justify-content-between"
+    assert_equal expected, result
   end
 end
