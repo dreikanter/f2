fe1b7eff87db1de9b5347e4ca55998ed1112fa0a
Alex Musayev <alex.musayev@gmail.com>
2025-09-25 10:21:28 +0200

    Initialize password_updated_at on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validates :email_address, presence: true, uniqueness: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
+  before_create :set_password_updated_at
+
   generates_token_for :password_reset, expires_in: 15.minutes do
     password_salt&.last(10)
   end
@@ -20,5 +22,11 @@ class User < ApplicationRecord
 
   def permission?(permission_name)
     permissions.exists?(name: permission_name)
+  end
+
+  private
+
+  def set_password_updated_at
+    self.password_updated_at = Time.current
   end
 end
