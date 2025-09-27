a1249ab7855c886ce22a07dba7922ebf552e5dd1
Alex Musayev <alex.musayev@gmail.com>
2025-09-27 12:10:02 +0200

    Add Pagination concern for reusable pagination logic

---
+++ app/controllers/concerns/pagination.rb
@@ -0,0 +1,40 @@
+module Pagination
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :pagination_total_pages, :pagination_current_page, :pagination_per_page, :pagination_total_count
+  end
+
+  private
+
+  attr_reader :pagination_total_count, :pagination_current_page, :pagination_total_pages
+
+  def paginate_scope(scope = nil)
+    scope ||= pagination_scope
+    page = (params[:page] || 1).to_i
+    offset = pagination_offset(page)
+
+    @pagination_total_count = scope.count
+    @pagination_current_page = page
+    @pagination_total_pages = (@pagination_total_count.to_f / pagination_per_page).ceil
+
+    scope.limit(pagination_per_page).offset(offset)
+  end
+
+  def pagination_offset(page = pagination_current_page)
+    (page - 1) * pagination_per_page
+  end
+
+  def pagination_per_page
+    self.class.const_defined?(:PER_PAGE) ? self.class::PER_PAGE : default_per_page
+  end
+
+  def default_per_page
+    25
+  end
+
+  # Override this method in the including controller to provide the scope to paginate
+  def pagination_scope
+    raise NotImplementedError, "Controllers using Pagination must implement pagination_scope method"
+  end
+end
