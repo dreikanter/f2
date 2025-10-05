ca99c2fcb1a6e0085df8391c06b1901f72dc6127
Alex Musayev <alex.musayev@gmail.com>
2025-10-05 16:39:11 +0200

    Check if access_token is active in PostWithdrawalJob

--- app/jobs/post_withdrawal_job.rb
+++ app/jobs/post_withdrawal_job.rb
@@ -5,7 +5,10 @@ class PostWithdrawalJob < ApplicationJob
     post = Post.find_by(id: post_id)
     return unless post
 
-    client = post.feed.access_token.build_client
+    access_token = post.feed.access_token
+    return unless access_token&.active?
+
+    client = access_token.build_client
     client.delete_post(post.freefeed_post_id)
   rescue FreefeedClient::Error => e
     Rails.logger.error("Failed to withdraw post #{post_id} from FreeFeed: #{e.message}")
