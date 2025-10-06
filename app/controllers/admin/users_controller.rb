ce2b43b85c8d49341f6fa88f61234082cc5f4762
Alex Musayev <alex.musayev@gmail.com>
2025-10-06 22:54:26 +0200

    Fix N+1

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,6 +14,23 @@ class Admin::UsersController < ApplicationController
   private
 
   def pagination_scope
-    policy_scope(User).order(created_at: :desc)
+    base_scope
+      .left_joins(:feeds, :access_tokens, :sessions)
+      .left_joins(feeds: :posts)
+      .group("users.id")
+      .select("users.*,
+               COUNT(DISTINCT feeds.id) AS feeds_count,
+               COUNT(DISTINCT access_tokens.id) AS access_tokens_count,
+               COUNT(DISTINCT posts.id) AS posts_count,
+               MAX(sessions.updated_at) AS last_seen_at")
+      .order(created_at: :desc)
+  end
+
+  def pagination_total_count
+    @pagination_total_count ||= base_scope.count
+  end
+
+  def base_scope
+    policy_scope(User)
   end
 end
