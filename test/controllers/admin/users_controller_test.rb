cc32cdc9d530aff485980160fdc8746c7e889c41
Alex Musayev <alex.musayev@gmail.com>
2025-10-06 23:21:10 +0200

    Simplify users index page tests

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -57,105 +57,13 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should paginate users" do
     login_as(admin_user)
 
-    30.times do |i|
-      create(:user, email_address: "user#{i}_#{rand(10000)}@example.com")
-    end
+    30.times { create(:user) }
 
     get admin_users_path
 
     assert_response :success
     assert_select ".pagination"
     assert_select "tbody tr", count: 25
-  end
-
-  test "should display user statistics on show page" do
-    login_as(admin_user)
-    user = create(:user)
-    feed1 = create(:feed, user: user, state: :enabled)
-    feed2 = create(:feed, user: user, state: :disabled)
-    token1 = create(:access_token, user: user, status: "active")
-    token2 = create(:access_token, user: user, status: "inactive")
-    post1 = create(:post, feed: feed1)
-
-    get admin_user_path(user)
-
-    assert_response :success
-    assert_select "strong", text: "Feeds:"
-    assert_select "strong", text: "Access Tokens:"
-    assert_select "strong", text: "Posts:"
-  end
-
-  test "should show permissions on user details" do
-    login_as(admin_user)
-    regular = create(:user)
-
-    get admin_user_path(admin_user)
-    assert_response :success
-    assert_select "strong", text: "Permissions:"
-
-    get admin_user_path(regular)
-    assert_response :success
-    assert_select "strong", text: "Permissions:"
-  end
-
-  test "should show password updated timestamp" do
-    login_as(admin_user)
-    user = create(:user)
-
-    get admin_user_path(user)
-
-    assert_response :success
-    assert_select "strong", text: "Password Updated:"
-  end
-
-  test "should show active sessions" do
-    login_as(admin_user)
-    user = create(:user)
-
-    get admin_user_path(user)
-
-    assert_response :success
-    assert_select "h3", text: "Active Sessions"
-  end
-
-  test "should show email change link" do
-    login_as(admin_user)
-    user = create(:user)
-
-    get admin_user_path(user)
-
-    assert_response :success
-    assert_select "a[href='#{edit_admin_user_email_update_path(user)}']", text: "Change Email"
-  end
-
-  test "should show password reset link" do
-    login_as(admin_user)
-    user = create(:user)
-
-    get admin_user_path(user)
-
-    assert_response :success
-    assert_select "a[href='#{admin_user_password_reset_path(user)}']", text: "Reset Password"
-  end
-
-  test "should show last seen timestamp" do
-    login_as(admin_user)
-    user = create(:user)
-
-    get admin_user_path(user)
-
-    assert_response :success
-    assert_select "strong", text: "Last Seen:"
-  end
-
-  test "should show last seen in users index" do
-    login_as(admin_user)
-    user = create(:user)
-
-    get admin_users_path
-
-    assert_response :success
-    assert_select "th", text: "Last Seen"
   end
 
   private
