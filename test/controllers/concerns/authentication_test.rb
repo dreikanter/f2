699bf881b20d3be6d71fbc3ca713b2366be21b8e
Alex Musayev <alex.musayev@gmail.com>
2025-10-06 23:10:21 +0200

    Test coverage for last seen timestamp tracking

--- test/controllers/concerns/authentication_test.rb
+++ test/controllers/concerns/authentication_test.rb
@@ -3,10 +3,9 @@ require "test_helper"
 class AuthenticationTest < ActionDispatch::IntegrationTest
   test "authenticated? returns true when user is signed in" do
     user = create(:user)
-    post session_url, params: { email_address: user.email_address, password: "password123" }
+    login_as(user)
     follow_redirect!
 
-    # Now test a protected action
     get feeds_path
     assert_response :success
   end
@@ -14,5 +13,38 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
   test "require_authentication redirects when not signed in" do
     get feeds_path
     assert_redirected_to new_session_path
+  end
+
+  test "update_session_activity touches session after 10 minutes" do
+    user = create(:user)
+    login_as(user)
+    session = user.sessions.last
+
+    session.update_column(:updated_at, 11.minutes.ago)
+
+    get feeds_path
+    assert_response :success
+
+    assert session.reload.updated_at > 1.minute.ago
+  end
+
+  test "update_session_activity does not touch recent session" do
+    user = create(:user)
+    login_as(user)
+    session = user.sessions.last
+
+    session.update_column(:updated_at, 5.minutes.ago)
+    old_timestamp = session.updated_at
+
+    get feeds_path
+    assert_response :success
+
+    assert_equal old_timestamp.to_i, session.reload.updated_at.to_i
+  end
+
+  private
+
+  def login_as(user)
+    post session_url, params: { email_address: user.email_address, password: "password123" }
   end
 end
