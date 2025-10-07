bb0b3272050257eac1ca0b806d29e014b0969e23
Alex Musayev <alex.musayev@gmail.com>
2025-10-07 21:35:26 +0200

    Prevent suspended users from signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
-      start_new_session_for user
-      redirect_to after_authentication_url
+      if user.suspended?
+        redirect_to new_session_path, alert: "Your account has been suspended."
+      else
+        start_new_session_for user
+        redirect_to after_authentication_url
+      end
     else
       redirect_to new_session_path, alert: "Try another email address or password."
     end
