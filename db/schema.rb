0c8034b2937d1e3d7ad9bf34128c76e4578f0f08
Alex Musayev <alex.musayev@gmail.com>
2025-10-07 21:21:36 +0200

    Add suspended_at column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_06_224951) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_07_192103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -283,6 +283,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_06_224951) do
     t.string "email_address", null: false
     t.string "password_digest", null: false
     t.datetime "password_updated_at", precision: nil, null: false
+    t.datetime "suspended_at"
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
