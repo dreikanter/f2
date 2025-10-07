427c0e255e71cd233b7f68395ace5286deb3d6fc
Alex Musayev <alex.musayev@gmail.com>
2025-10-07 21:53:27 +0200

    Use local variables in UserSuspensionsController

--- app/controllers/admin/user_suspensions_controller.rb
+++ app/controllers/admin/user_suspensions_controller.rb
@@ -1,15 +1,15 @@
 class Admin::UserSuspensionsController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
-    authorize @user
-    @user.suspend!
-    redirect_to admin_user_path(@user), notice: "User has been suspended."
+    user = User.find(params[:user_id])
+    authorize user
+    user.suspend!
+    redirect_to admin_user_path(user), notice: "User has been suspended."
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    authorize @user
-    @user.unsuspend!
-    redirect_to admin_user_path(@user), notice: "User has been unsuspended."
+    user = User.find(params[:user_id])
+    authorize user
+    user.unsuspend!
+    redirect_to admin_user_path(user), notice: "User has been unsuspended."
   end
 end
