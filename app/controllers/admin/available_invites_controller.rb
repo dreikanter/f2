30f47be2364e2ecf6f92810f4ac1526e2426873e
Alex Musayev <alex.musayev@gmail.com>
2025-10-08 23:56:33 +0200

    Safer params processing

--- app/controllers/admin/available_invites_controller.rb
+++ app/controllers/admin/available_invites_controller.rb
@@ -3,7 +3,7 @@ class Admin::AvailableInvitesController < ApplicationController
     user = User.find(params[:user_id])
     authorize user
 
-    if user.update(available_invites_params)
+    if user.update(available_invites: available_invites)
       redirect_to admin_user_path(user), notice: "Available invites updated successfully."
     else
       redirect_to admin_user_path(user), alert: "Failed to update available invites."
@@ -11,6 +11,10 @@ class Admin::AvailableInvitesController < ApplicationController
   end
 
   private
+
+  def available_invites
+    Integer(available_invites_params[:available_invites])
+  end
 
   def available_invites_params
     params.require(:user).permit(:available_invites)
