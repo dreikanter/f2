ad28254ce550e93361e89f1f69e038d2af468067
Alex Musayev <alex.musayev@gmail.com>
2025-10-08 18:37:31 +0200

    Fix available invites update with strong parameters

--- app/controllers/admin/available_invites_controller.rb
+++ app/controllers/admin/available_invites_controller.rb
@@ -3,10 +3,16 @@ class Admin::AvailableInvitesController < ApplicationController
     user = User.find(params[:user_id])
     authorize user
 
-    if user.update(available_invites: params[:available_invites])
+    if user.update(available_invites_params)
       redirect_to admin_user_path(user), notice: "Available invites updated successfully."
     else
       redirect_to admin_user_path(user), alert: "Failed to update available invites."
     end
+  end
+
+  private
+
+  def available_invites_params
+    params.require(:user).permit(:available_invites)
   end
 end
