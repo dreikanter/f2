19e775c0335b40638cc2b0d66fddf0fe00844adb
Alex Musayev <alex.musayev@gmail.com>
2025-10-09 11:46:11 +0200

    Refactor invites controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,24 +1,18 @@
 class InvitesController < ApplicationController
   def index
     authorize Invite
-    @invites = policy_scope(Invite).includes(:invited_user).order(created_at: :desc)
-    @available_invites_count = Current.user.available_invites
-    @created_invites_count = @invites.count
-    @invited_users_count = @invites.where.not(invited_user_id: nil).count
-    @unused_invites_count = @invites.where(invited_user_id: nil).count
+    @invites = ordered_invites
+    @invite_stats = invite_stats
   end
 
   def create
-    Invite.transaction do
-      authorize Invite
-      Current.user.created_invites.create!
-    end
-
+    authorize Invite
+    create_invite_if_possible
     render_invite_updates
   end
 
   def destroy
-    invite = Current.user.created_invites.find(params[:id])
+    invite = created_invites.find(params[:id])
     authorize invite
     invite.destroy!
 
@@ -27,24 +21,43 @@ class InvitesController < ApplicationController
 
   private
 
+  def create_invite_if_possible
+    Invite.transaction do
+      created_invites_count = created_invites.count
+      created_invites.create! if available_invites_count > created_invites_count
+    end
+  end
+
   def render_invite_updates
-    invites = policy_scope(Invite).includes(:invited_user).order(created_at: :desc)
+    invites = ordered_invites
 
     render turbo_stream: [
       turbo_stream.update("invites-table", partial: "invites/table", locals: { invites: invites }),
-      turbo_stream.update("invite-stats", partial: "invites/stats", locals: invite_stats(invites)),
+      turbo_stream.update("invite-stats", partial: "invites/stats", locals: invite_stats),
       turbo_stream.update("create-invite-button", partial: "invites/create_button", locals: {
         can_create: policy(Invite).create?
       })
     ]
   end
 
-  def invite_stats(invites)
+  def invite_stats
     {
-      available_invites_count: Current.user.available_invites,
-      created_invites_count: invites.count,
-      invited_users_count: invites.where.not(invited_user_id: nil).count,
-      unused_invites_count: invites.where(invited_user_id: nil).count
+      available_invites_count: available_invites_count,
+      created_invites_count: created_invites.count,
+      invited_users_count: created_invites.where.not(invited_user_id: nil).count,
+      unused_invites_count: created_invites.where(invited_user_id: nil).count
     }
+  end
+
+  def available_invites_count
+    Current.user.available_invites
+  end
+
+  def created_invites
+    Current.user.created_invites
+  end
+
+  def ordered_invites
+    policy_scope(Invite).includes(:invited_user).order(created_at: :desc)
   end
 end
