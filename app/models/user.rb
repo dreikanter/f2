15ae6f63a2a706072d0d2c80159aa72ae536fef5
Alex Musayev <alex.musayev@gmail.com>
2025-10-10 21:46:40 +0200

    Simplify pwd timestamping

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,7 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  before_create :set_password_updated_at
-  before_update :update_password_timestamp, if: :will_save_change_to_password_digest?
+  before_save :set_password_updated_at, if: :will_save_change_to_password_digest?
 
   generates_token_for :password_reset, expires_in: 15.minutes do
     password_salt&.last(10)
@@ -51,10 +50,6 @@ class User < ApplicationRecord
   private
 
   def set_password_updated_at
-    self.password_updated_at = Time.current
-  end
-
-  def update_password_timestamp
     self.password_updated_at = Time.current
   end
 end
