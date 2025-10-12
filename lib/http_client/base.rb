d953925a5358e12080f03b3ffcae1dba8d7fa70b
Alex Musayev <alex.musayev@gmail.com>
2025-10-12 15:17:59 +0200

    Refactor HTTP client

--- lib/http_client/base.rb
+++ lib/http_client/base.rb
@@ -1,5 +1,11 @@
 module HttpClient
   class Base
+    attr_reader :client_options
+
+    def initialize(options = {})
+      @client_options = options.freeze
+    end
+
     def get(url, headers: {}, options: {})
       raise NotImplementedError, "Subclasses must implement #get"
     end
@@ -14,6 +20,17 @@ module HttpClient
 
     def delete(url, headers: {}, options: {})
       raise NotImplementedError, "Subclasses must implement #delete"
+    end
+
+    # Override this in subclasses to provide default options
+    def default_options
+      {}
+    end
+
+    # Merges default options with client options and per-request options
+    # Returns a new hash with the merged options
+    def options(request_options = {})
+      default_options.merge(client_options).merge(request_options)
     end
   end
 end
