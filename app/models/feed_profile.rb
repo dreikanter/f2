da528e5a7ac710cd972eadb206f1b56501707027
Alex Musayev <alex.musayev@gmail.com>
2025-10-13 16:01:03 +0200

    Use full class namesinstead of resolving

--- app/models/feed_profile.rb
+++ app/models/feed_profile.rb
@@ -2,8 +2,18 @@
 #
 class FeedProfile
   PROFILES = {
-    "rss" => { loader: "http", processor: "rss", normalizer: "rss", title_extractor: "rss" },
-    "xkcd" => { loader: "http", processor: "rss", normalizer: "xkcd", title_extractor: "rss" }
+    "rss" => {
+      loader: "Loader::HttpLoader",
+      processor: "Processor::RssProcessor",
+      normalizer: "Normalizer::RssNormalizer",
+      title_extractor: "TitleExtractor::RssTitleExtractor"
+    },
+    "xkcd" => {
+      loader: "Loader::HttpLoader",
+      processor: "Processor::RssProcessor",
+      normalizer: "Normalizer::XkcdNormalizer",
+      title_extractor: "TitleExtractor::RssTitleExtractor"
+    }
   }.freeze
 
   # Returns all available profile keys
@@ -23,27 +33,39 @@ class FeedProfile
   # @param key [String] the profile key
   # @return [Class] the loader class
   def self.loader_class_for(key)
-    ClassResolver.resolve("Loader", PROFILES.dig(key, :loader))
+    class_name = PROFILES.dig(key, :loader)
+    raise ArgumentError, "Profile '#{key}' not found" unless class_name
+
+    class_name.constantize
   end
 
   # Resolves and returns the processor class for a given profile key
   # @param key [String] the profile key
   # @return [Class] the processor class
   def self.processor_class_for(key)
-    ClassResolver.resolve("Processor", PROFILES.dig(key, :processor))
+    class_name = PROFILES.dig(key, :processor)
+    raise ArgumentError, "Profile '#{key}' not found" unless class_name
+
+    class_name.constantize
   end
 
   # Resolves and returns the normalizer class for a given profile key
   # @param key [String] the profile key
   # @return [Class] the normalizer class
   def self.normalizer_class_for(key)
-    ClassResolver.resolve("Normalizer", PROFILES.dig(key, :normalizer))
+    class_name = PROFILES.dig(key, :normalizer)
+    raise ArgumentError, "Profile '#{key}' not found" unless class_name
+
+    class_name.constantize
   end
 
   # Resolves and returns the title extractor class for a given profile key
   # @param key [String] the profile key
   # @return [Class] the title extractor class
   def self.title_extractor_class_for(key)
-    ClassResolver.resolve("TitleExtractor", PROFILES.dig(key, :title_extractor))
+    class_name = PROFILES.dig(key, :title_extractor)
+    raise ArgumentError, "Profile '#{key}' not found" unless class_name
+
+    class_name.constantize
   end
 end
