b2343700131dd5c6fbecece88b416a3de1b37291
Alex Musayev <alex.musayev@gmail.com>
2025-10-16 19:56:51 +0200

    Implement the onboarding wizard

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -1,15 +1,47 @@
 class OnboardingsController < ApplicationController
   skip_onboarding_redirect
 
+  def show
+    @onboarding = Current.user.onboarding
+    redirect_to redirect_path_after_onboarding unless @onboarding
+  end
+
   def create
     Current.user.create_onboarding!
     session[:onboarding] = true
     redirect_to onboarding_path
   end
 
+  def update
+    @onboarding = Current.user.onboarding
+    return redirect_to redirect_path_after_onboarding unless @onboarding
+
+    if @onboarding.last_step?
+      complete_onboarding
+    else
+      @onboarding.update!(current_step: @onboarding.next_step)
+      redirect_to onboarding_path
+    end
+  end
+
   def destroy
+    clear_onboarding
+    redirect_to redirect_path_after_onboarding
+  end
+
+  private
+
+  def clear_onboarding
     Current.user.onboarding&.destroy
     session[:onboarding] = false
-    redirect_to status_path
+  end
+
+  def complete_onboarding
+    clear_onboarding
+    redirect_to redirect_path_after_onboarding, notice: "Setup complete. Your feed is ready to go."
+  end
+
+  def redirect_path_after_onboarding
+    status_path
   end
 end
--- app/models/onboarding.rb
+++ app/models/onboarding.rb
@@ -1,3 +1,38 @@
 class Onboarding < ApplicationRecord
   belongs_to :user
+
+  enum :current_step, { introduction: 0, token: 1, feed: 2, schedule: 3, finalization: 4 }
+
+  def next_step
+    return nil if finalization?
+
+    steps = ordered_steps
+    current_index = steps.index(current_step)
+    return nil if current_index.nil? || current_index >= steps.length - 1
+
+    steps[current_index + 1]
+  end
+
+  def step_number
+    current_index = ordered_steps.index(current_step)
+    current_index ? current_index + 1 : nil
+  end
+
+  def total_steps
+    self.class.current_steps.length
+  end
+
+  def first_step?
+    introduction?
+  end
+
+  def last_step?
+    finalization?
+  end
+
+  private
+
+  def ordered_steps
+    self.class.current_steps.keys.map(&:to_s)
+  end
 end
