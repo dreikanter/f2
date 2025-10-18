f8a303ca12ccf2cea90da688819ce7249452537e
Alex Musayev <alex.musayev@gmail.com>
2025-10-19 00:31:55 +0200

    Test coverage

--- test/controllers/onboardings_controller_test.rb
+++ test/controllers/onboardings_controller_test.rb
@@ -11,6 +11,23 @@ class OnboardingsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should redirect to status when onboarding does not exist" do
+    user_without_onboarding = create(:user)
+    sign_in_as(user_without_onboarding)
+
+    get onboarding_url
+    assert_redirected_to status_path
+  end
+
+  test "should clear session flag when onboarding does not exist" do
+    user_without_onboarding = create(:user)
+    sign_in_as(user_without_onboarding)
+    session[:onboarding] = true
+
+    get onboarding_url
+    assert_equal false, session[:onboarding]
+  end
+
   test "should destroy onboarding" do
     sign_in_as(user)
     assert_not_nil user.onboarding
@@ -20,8 +37,85 @@ class OnboardingsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to status_path
   end
 
+  test "should clear session flag when destroying onboarding" do
+    sign_in_as(user)
+    session[:onboarding] = true
+
+    delete onboarding_url
+    assert_equal false, session[:onboarding]
+  end
+
+  test "should handle destroy when onboarding does not exist" do
+    user_without_onboarding = create(:user)
+    sign_in_as(user_without_onboarding)
+
+    delete onboarding_url
+    assert_redirected_to status_path
+  end
+
   test "should require authentication to access onboarding" do
     get onboarding_url
     assert_redirected_to new_session_path
+  end
+
+  test "should require authentication to destroy onboarding" do
+    delete onboarding_url
+    assert_redirected_to new_session_path
+  end
+
+  test "should show intro step when no access_token" do
+    sign_in_as(user)
+    get onboarding_url
+    assert_response :success
+    assert_select "h1", "Welcome to Feeder"
+  end
+
+  test "should show feed step when access_token exists but no feed" do
+    user_with_token = create(:user)
+    access_token = create(:access_token, user: user_with_token)
+    onboarding = create(:onboarding, user: user_with_token, access_token: access_token)
+    sign_in_as(user_with_token)
+
+    get onboarding_url
+    assert_response :success
+    assert_select "h1", "Token added"
+  end
+
+  test "should show outro step when both access_token and feed exist" do
+    user_with_both = create(:user)
+    access_token = create(:access_token, user: user_with_both)
+    feed = create(:feed, user: user_with_both)
+    onboarding = create(:onboarding, user: user_with_both, access_token: access_token, feed: feed)
+    sign_in_as(user_with_both)
+
+    get onboarding_url
+    assert_response :success
+    assert_select "h1", "You're all set"
+  end
+
+  test "should override step with step parameter 1" do
+    user_with_both = create(:user)
+    access_token = create(:access_token, user: user_with_both)
+    feed = create(:feed, user: user_with_both)
+    onboarding = create(:onboarding, user: user_with_both, access_token: access_token, feed: feed)
+    sign_in_as(user_with_both)
+
+    get onboarding_url(step: 1)
+    assert_response :success
+    assert_select "h1", "Welcome to Feeder"
+  end
+
+  test "should override step with step parameter 2" do
+    sign_in_as(user)
+    get onboarding_url(step: 2)
+    assert_response :success
+    assert_select "h1", "Token added"
+  end
+
+  test "should override step with step parameter 3" do
+    sign_in_as(user)
+    get onboarding_url(step: 3)
+    assert_response :success
+    assert_select "h1", "You're all set"
   end
 end
--- test/models/onboarding_test.rb
+++ test/models/onboarding_test.rb
@@ -44,4 +44,86 @@ class OnboardingTest < ActiveSupport::TestCase
 
     assert_equal feed, onboarding.feed
   end
+
+  test "can be associated with both access_token and feed" do
+    user = create(:user)
+    access_token = create(:access_token, user: user)
+    feed = create(:feed, user: user)
+    onboarding = Onboarding.create!(user: user, access_token: access_token, feed: feed)
+
+    assert_equal access_token, onboarding.access_token
+    assert_equal feed, onboarding.feed
+  end
+
+  test "token_setup? returns true when no access_token" do
+    user = create(:user)
+    onboarding = Onboarding.create!(user: user)
+
+    assert onboarding.token_setup?
+  end
+
+  test "token_setup? returns false when access_token exists" do
+    user = create(:user)
+    access_token = create(:access_token, user: user)
+    onboarding = Onboarding.create!(user: user, access_token: access_token)
+
+    assert_not onboarding.token_setup?
+  end
+
+  test "feed_setup? returns false when no access_token" do
+    user = create(:user)
+    onboarding = Onboarding.create!(user: user)
+
+    assert_not onboarding.feed_setup?
+  end
+
+  test "feed_setup? returns true when access_token exists but no feed" do
+    user = create(:user)
+    access_token = create(:access_token, user: user)
+    onboarding = Onboarding.create!(user: user, access_token: access_token)
+
+    assert onboarding.feed_setup?
+  end
+
+  test "feed_setup? returns false when both access_token and feed exist" do
+    user = create(:user)
+    access_token = create(:access_token, user: user)
+    feed = create(:feed, user: user)
+    onboarding = Onboarding.create!(user: user, access_token: access_token, feed: feed)
+
+    assert_not onboarding.feed_setup?
+  end
+
+  test "token_setup? uses access_token_id directly" do
+    user = create(:user)
+    access_token = create(:access_token, user: user)
+    onboarding = Onboarding.create!(user: user, access_token: access_token)
+
+    # Manually set access_token_id to nil without loading association
+    onboarding.update_column(:access_token_id, nil)
+    assert onboarding.token_setup?
+  end
+
+  test "feed_setup? uses access_token_id and feed_id directly" do
+    user = create(:user)
+    access_token = create(:access_token, user: user)
+    onboarding = Onboarding.create!(user: user, access_token: access_token)
+
+    # Manually set feed_id to nil without loading association
+    onboarding.update_column(:feed_id, nil)
+    assert onboarding.feed_setup?
+  end
+
+  test "belongs_to user" do
+    user = create(:user)
+    onboarding = Onboarding.create!(user: user)
+
+    assert_equal user, onboarding.user
+  end
+
+  test "requires user" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Onboarding.create!(user: nil)
+    end
+  end
 end
