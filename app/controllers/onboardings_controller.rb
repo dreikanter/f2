37884674ee60664aac71fe46562e1700f36b29c1
Alex Musayev <alex.musayev@gmail.com>
2025-10-19 00:03:30 +0200

    Implement 3-step setup

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -3,10 +3,30 @@ class OnboardingsController < ApplicationController
 
   def show
     @onboarding = Current.user.onboarding
+    @current_step = current_step
   end
 
   def destroy
     Current.user.onboarding&.destroy
+    session[:onboarding] = false
     redirect_to status_path
+  end
+
+  private
+
+  def current_step
+    case params[:step]
+    when "1" then :intro
+    when "2" then :feed
+    when "3" then :outro
+    else
+      if @onboarding.token_setup?
+        :intro
+      elsif @onboarding.feed_setup?
+        :feed
+      else
+        :outro
+      end
+    end
   end
 end
--- app/models/onboarding.rb
+++ app/models/onboarding.rb
@@ -2,4 +2,12 @@ class Onboarding < ApplicationRecord
   belongs_to :user
   belongs_to :access_token, optional: true
   belongs_to :feed, optional: true
+
+  def token_setup?
+    access_token_id.nil?
+  end
+
+  def feed_setup?
+    access_token_id.present? && feed_id.nil?
+  end
 end
