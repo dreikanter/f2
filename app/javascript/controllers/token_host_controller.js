5a692ec99a39c54bedfe9ea3b1d2c7382d9247da
Alex Musayev <alex.musayev@gmail.com>
2025-10-19 17:23:12 +0200

    Add defensive check for link target in token-host controller

--- app/javascript/controllers/token_host_controller.js
+++ app/javascript/controllers/token_host_controller.js
@@ -12,6 +12,10 @@ export default class extends Controller {
   }
 
   updateLink() {
+    if (!this.hasLinkTarget) {
+      return
+    }
+
     const selectedUrl = this.hostSelectTarget.value
     const hostConfig = Object.values(this.hostsValue).find(h => h.url === selectedUrl)
 
