1fc6433506c1b3cd1e299a916ba32c54d006029e
Alex Musayev <alex.musayev@gmail.com>
2025-10-19 22:12:32 +0200

    Add caching for system stats page

--- app/controllers/admin/system_stats_controller.rb
+++ app/controllers/admin/system_stats_controller.rb
@@ -1,6 +1,8 @@
 class Admin::SystemStatsController < ApplicationController
   def show
     authorize :admin, :show?
-    @disk_usage = DiskUsageService.new.call
+    @disk_usage = Rails.cache.fetch("admin/system_stats", expires_in: 5.minutes) do
+      DiskUsageService.new.call
+    end
   end
 end
