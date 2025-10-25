a8ca4af5975bde73c786ce783437737fc1e0f081
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 21:14:42 +0200

    Prevent sending emails to deactivated addresses

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -1,5 +1,7 @@
 class PasswordsMailer < ApplicationMailer
   def reset(user)
+    return if user.email_deactivated?
+
     @user = user
     mail subject: "Reset your password", to: user.email_address
   end
