1257cfc3a65e03d6ea0574ba8c50e45379bff9a6
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 20:01:51 +0200

    Update tests to expect title case headings

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -21,20 +21,20 @@ class RegistrationsControllerTest < ActionDispatch::IntegrationTest
   test "should show invalid invitation message when code is invalid" do
     get registration_url(code: "invalid-uuid")
     assert_response :success
-    assert_select "h1", "Incorrect invitation code"
+    assert_select "h1", "Incorrect Invitation Code"
     assert_select "p", /not valid/
   end
 
   test "should show registration form with valid unused invite" do
     get registration_url(code: invite.id)
     assert_response :success
-    assert_select "h1", "Create your account"
+    assert_select "h1", "Create Your Account"
   end
 
   test "should show used invite message when invite is already used" do
     get registration_url(code: used_invite.id)
     assert_response :success
-    assert_select "h1", "Invitation already used"
+    assert_select "h1", "Invitation Already Used"
     assert_select "p", /already been used to create an account/
   end
 
