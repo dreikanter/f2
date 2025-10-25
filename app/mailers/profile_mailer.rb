07d35a04dcb21501a275cc5871bad8d151e6f12f
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 11:26:08 +0200

    Add account_confirmation mailer method and views

--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -7,4 +7,12 @@ class ProfileMailer < ApplicationMailer
 
     mail(to: @new_email, subject: "Confirm your new email address")
   end
+
+  def account_confirmation(user)
+    @user = user
+    @token = user.generate_token_for(:email_confirmation)
+    @confirmation_url = email_confirmation_url(@token)
+
+    mail(to: user.email_address, subject: "Confirm your email address")
+  end
 end
