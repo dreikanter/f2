031faf9966f41b717d90648155c177a25151991b
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 11:31:12 +0200

    Add tests for account activation in EmailConfirmationsController

--- test/controllers/email_confirmations_controller_test.rb
+++ test/controllers/email_confirmations_controller_test.rb
@@ -44,4 +44,32 @@ class EmailConfirmationsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to settings_path
     assert_equal "Email confirmation failed. The email may already be taken.", flash[:alert]
   end
+
+  test "should activate inactive user with valid confirmation token" do
+    inactive_user = create(:user, state: :inactive)
+    token = inactive_user.generate_token_for(:email_confirmation)
+
+    get email_confirmation_url(token)
+
+    assert_redirected_to new_session_path
+    assert_equal "Your email is now confirmed and the account is activated. Please sign in.", flash[:notice]
+    assert inactive_user.reload.active?
+  end
+
+  test "should redirect to login with invalid confirmation token" do
+    get email_confirmation_url("invalid")
+
+    assert_redirected_to new_session_path
+    assert_equal "Email confirmation link is invalid or has expired.", flash[:alert]
+  end
+
+  test "should not activate already active user" do
+    active_user = create(:user, state: :active)
+    token = active_user.generate_token_for(:email_confirmation)
+
+    get email_confirmation_url(token)
+
+    assert_redirected_to new_session_path
+    assert active_user.reload.active?
+  end
 end
