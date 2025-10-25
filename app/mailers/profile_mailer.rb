6a492dde0da607a06af0811829badbe544488a8a
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 13:05:27 +0200

    Update ProfileMailer to use new email confirmation URLs

--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -3,7 +3,7 @@ class ProfileMailer < ApplicationMailer
     @user = user
     @new_email = user.unconfirmed_email
     @token = user.generate_token_for(:change_email_confirmation)
-    @confirmation_url = email_confirmation_url(@token)
+    @confirmation_url = settings_email_confirmation_url(@token)
 
     mail(to: @new_email, subject: "Confirm your new email address")
   end
@@ -11,7 +11,7 @@ class ProfileMailer < ApplicationMailer
   def account_confirmation(user)
     @user = user
     @token = user.generate_token_for(:initial_email_confirmation)
-    @confirmation_url = email_confirmation_url(@token)
+    @confirmation_url = registration_email_confirmation_url(@token)
 
     mail(to: user.email_address, subject: "Confirm your email address")
   end
