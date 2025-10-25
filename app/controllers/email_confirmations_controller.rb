1d1a00cc2c282de7ffa585ce3055870eae429f01
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 11:27:56 +0200

    Handle account activation in EmailConfirmationsController

--- app/controllers/email_confirmations_controller.rb
+++ app/controllers/email_confirmations_controller.rb
@@ -1,10 +1,15 @@
 class EmailConfirmationsController < ApplicationController
+  allow_unauthenticated_access
+
   def show
     user = find_user_by_token
 
-    if valid_email_change?
+    if account_confirmation?
+      activate_user(user)
+      redirect_with_account_activated
+    elsif valid_email_change?
       update_user_email(user)
-      redirect_with_success
+      redirect_with_email_updated
     else
       redirect_with_failure
     end
@@ -15,11 +20,17 @@ class EmailConfirmationsController < ApplicationController
   private
 
   def find_user_by_token
-    User.find_by_token_for!(:email_change, params[:token])
+    # Try email_confirmation token first, then email_change token
+    User.find_by_token_for(:email_confirmation, params[:token]) ||
+      User.find_by_token_for!(:email_change, params[:token])
   end
 
   def new_email
     params[:new_email]
+  end
+
+  def account_confirmation?
+    new_email.blank?
   end
 
   def valid_email_change?
@@ -30,11 +41,19 @@ class EmailConfirmationsController < ApplicationController
     User.exists?(email_address: new_email)
   end
 
+  def activate_user(user)
+    user.update!(state: :active) if user.inactive?
+  end
+
   def update_user_email(user)
     user.update!(email_address: new_email)
   end
 
-  def redirect_with_success
+  def redirect_with_account_activated
+    redirect_to new_session_path, notice: "Your email is now confirmed and the account is activated. Please sign in."
+  end
+
+  def redirect_with_email_updated
     redirect_to settings_path, notice: "Email address successfully updated to #{new_email}."
   end
 
@@ -43,6 +62,6 @@ class EmailConfirmationsController < ApplicationController
   end
 
   def redirect_with_invalid_token
-    redirect_to settings_path, alert: "Email confirmation link is invalid or has expired."
+    redirect_to new_session_path, alert: "Email confirmation link is invalid or has expired."
   end
 end
