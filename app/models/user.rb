ea5f1a536b0e9960cc4fd5486e37153da32f7c8d
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 12:01:54 +0200

    Update email_change token to 24 hours and remove email_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,11 +25,7 @@ class User < ApplicationRecord
     password_salt&.last(10)
   end
 
-  generates_token_for :email_change, expires_in: 15.minutes do
-    email_address
-  end
-
-  generates_token_for :email_confirmation, expires_in: 2.hours do
+  generates_token_for :email_change, expires_in: 24.hours do
     email_address
   end
 
