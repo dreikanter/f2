9208f73a75c316882e45cc40d0bd6fc2e68249a1
Alex Musayev <alex.musayev@gmail.com>
2025-10-25 12:05:50 +0200

    Update ProfileMailer to use email_confirmation token

--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -2,7 +2,7 @@ class ProfileMailer < ApplicationMailer
   def email_change_confirmation(user, new_email)
     @user = user
     @new_email = new_email
-    @token = user.generate_token_for(:email_change)
+    @token = user.generate_token_for(:email_confirmation)
     @confirmation_url = email_confirmation_url(@token, new_email: @new_email)
 
     mail(to: @new_email, subject: "Confirm your new email address")
@@ -10,7 +10,7 @@ class ProfileMailer < ApplicationMailer
 
   def account_confirmation(user)
     @user = user
-    @token = user.generate_token_for(:email_change)
+    @token = user.generate_token_for(:email_confirmation)
     @confirmation_url = email_confirmation_url(@token)
 
     mail(to: user.email_address, subject: "Confirm your email address")
