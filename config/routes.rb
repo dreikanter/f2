c43e8800fa81f6752b82633992a5f9ab5cc6c3cc
Alex Musayev <alex.musayev@gmail.com>
2025-10-26 11:45:58 +0100

    Use proper resource definition for email reactivation

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
       resource :password_reset, only: [:show, :create]
       resource :suspension, only: [:create, :destroy], controller: "user_suspensions"
       resource :available_invites, only: :update, controller: "available_invites"
-      post :reactivate_email, on: :member
+      resource :email_reactivation, only: :create
     end
 
     resources :events, only: [:index, :show]
