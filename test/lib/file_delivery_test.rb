e5df2434b49f504fb2e9cac99645bed6ca4ad608
Alex Musayev <alex.musayev@gmail.com>
2025-10-27 11:48:42 +0100

    Refactor file delivery class test

--- test/lib/file_delivery_test.rb
+++ test/lib/file_delivery_test.rb
@@ -1,9 +1,12 @@
 require "test_helper"
 
 class FileDeliveryTest < ActiveSupport::TestCase
-  setup do
-    @email_storage = InMemoryEmailStorage.new
-    @delivery = FileDelivery.new(email_storage: @email_storage)
+  def email_storage
+    @email_storage ||= InMemoryEmailStorage.new
+  end
+
+  def delivery
+    @delivery ||= FileDelivery.new(email_storage: email_storage)
   end
 
   test "delivers email and saves to storage" do
@@ -14,16 +17,16 @@ class FileDeliveryTest < ActiveSupport::TestCase
       body "Test Body"
     end
 
-    @delivery.deliver!(mail)
+    delivery.deliver!(mail)
 
-    emails = @email_storage.list_emails
+    emails = email_storage.list_emails
     assert_equal 1, emails.size
 
     email = emails.first
     assert_match(/\d{8}_\d{6}_\d{3}_[0-9a-f-]{36}/, email[:id])
     assert_equal "Test Subject", email[:subject]
 
-    loaded = @email_storage.load_email(email[:id])
+    loaded = email_storage.load_email(email[:id])
     assert_equal "sender@example.com", loaded[:from]
     assert_equal "recipient@example.com", loaded[:to]
     assert_equal "Test Subject", loaded[:subject]
@@ -47,12 +50,12 @@ class FileDeliveryTest < ActiveSupport::TestCase
       end
     end
 
-    @delivery.deliver!(mail)
+    delivery.deliver!(mail)
 
-    emails = @email_storage.list_emails
+    emails = email_storage.list_emails
     assert_equal 1, emails.size
 
-    loaded = @email_storage.load_email(emails.first[:id])
+    loaded = email_storage.load_email(emails.first[:id])
     assert_equal true, loaded[:multipart]
     assert_equal "Text version", loaded[:text_part]
     assert_equal "<p>HTML version</p>", loaded[:html_part]
