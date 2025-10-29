69060702e00ff82cc982b504e6952e869fb71f2c
Alex Musayev <alex.musayev@gmail.com>
2025-10-29 14:44:54 +0100

    Separate application layout into new and legacy versions

---
+++ app/javascript/tailwind.js
@@ -0,0 +1,6 @@
+// Tailwind layout entrypoint shares the base Turbo/Stimulus boot logic and
+// gives us a spot for future Tailwind-only enhancements if we need them.
+//
+// TBD: After Tailwind migration is over, merge this file to application.js
+//
+import "./application"
