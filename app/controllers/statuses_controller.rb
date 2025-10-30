5d6e9dfc566caaa2cc8be5126673edfdbf00b42b
Alex Musayev <alex.musayev@gmail.com>
2025-10-30 12:48:38 +0100

    Refactor status page (#186)
    * Refactor status page
    
    * Rename CSS class
    
    * Define header classes
    
    * Refactor bg color definition for the body element
    
    * Use custom shortcut class for the headers
    
    * Ensure Flowbite components get re-initialized after Turbo navigation
    
    * Remove leftover Bootstrap classes from stats markup

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,4 +1,6 @@
 class StatusesController < ApplicationController
+  layout "tailwind"
+
   def show
     @user = Current.user
   end
--- app/javascript/tailwind.js
+++ app/javascript/tailwind.js
@@ -4,3 +4,4 @@
 // TBD: After Tailwind migration is over, merge this file to application.js
 //
 import "application"
+import "flowbite"
