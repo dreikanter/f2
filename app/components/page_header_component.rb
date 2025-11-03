923da9ba1a219ee3449c1533646c1a933f46a1fc
Alex Musayev <alex.musayev@gmail.com>
2025-11-03 14:00:28 +0100

    Introduce reusable page header component (#214)
    * Add PageHeaderComponent with slots for context and actions
    
    * Refactor feeds index to use PageHeaderComponent
    
    * Use renders_many for context paragraphs in PageHeaderComponent
    
    * Use with_context_paragraph in feeds index view
    
    * Use renders_many for action buttons in PageHeaderComponent
    
    * Use with_action_button in feeds index view
    
    * Formatting
    
    * Refactor feeds show to use PageHeaderComponent
    
    * Correct buttons justification
    
    * Correct whitespacing in the top part of the page
    
    * Remove redundant classes
    
    * Refactor settings show to use PageHeaderComponent
    
    * Refactor admin system stats to use PageHeaderComponent
    
    * Refactor posts index to use PageHeaderComponent
    
    * Refactor invites index to use PageHeaderComponent
    
    * Refactor access tokens index to use PageHeaderComponent
    
    * Unify whitespacing
    
    * Apply new component to the rest of the pages

---
+++ app/components/page_header_component.rb
@@ -0,0 +1,13 @@
+class PageHeaderComponent < ViewComponent::Base
+  renders_many :context_paragraphs, ->(content = nil, &block) do
+    content || block.call
+  end
+
+  renders_many :action_buttons, ->(content = nil, &block) do
+    content || block.call
+  end
+
+  def initialize(title:)
+    @title = title
+  end
+end
