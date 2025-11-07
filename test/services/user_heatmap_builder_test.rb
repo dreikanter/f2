ef88554e85208251b0eee864aa81abd85326ce10
Alex Musayev <alex.musayev@gmail.com>
2025-11-07 14:54:10 +0100

    Refactor heatmap integration tests

--- test/services/user_heatmap_builder_test.rb
+++ test/services/user_heatmap_builder_test.rb
@@ -29,56 +29,20 @@ class UserHeatmapBuilderTest < ActiveSupport::TestCase
     assert_includes svg, "</svg>"
   end
 
-  test "#build_cached should cache the SVG" do
-    create(:feed_metric, feed: feed, date: Date.current, posts_count: 5)
-
-    Rails.cache = ActiveSupport::Cache::MemoryStore.new
-
-    builder = UserHeatmapBuilder.new(user)
-    svg = builder.build_cached
-
-    cache_key = "user:#{user.id}:heatmap_svg:#{Date.current}"
-    cached_svg = Rails.cache.read(cache_key)
-
-    assert_not_nil cached_svg
-    assert_equal svg, cached_svg
-    assert_includes cached_svg, "<svg"
-  end
-
-  test "#build_cached should use cached SVG on second call" do
+  test "#build_cached should cache the result" do
     create(:feed_metric, feed: feed, date: Date.current, posts_count: 5)
 
     Rails.cache = ActiveSupport::Cache::MemoryStore.new
 
     builder = UserHeatmapBuilder.new(user)
     first_svg = builder.build_cached
+    second_svg = nil
 
-    # Modify the cache to verify second call uses it
-    cache_key = "user:#{user.id}:heatmap_svg:#{Date.current}"
-    Rails.cache.write(cache_key, "cached_content", expires_in: 24.hours)
-
-    second_svg = builder.build_cached
-
-    assert_equal "cached_content", second_svg
-    assert_not_equal first_svg, second_svg
-  end
-
-  test "#build_cached should expire cache after 24 hours" do
-    create(:feed_metric, feed: feed, date: Date.current, posts_count: 5)
-
-    Rails.cache = ActiveSupport::Cache::MemoryStore.new
-
-    builder = UserHeatmapBuilder.new(user)
-    builder.build_cached
-
-    cache_key = "user:#{user.id}:heatmap_svg:#{Date.current}"
-    assert_not_nil Rails.cache.read(cache_key), "Cache should exist initially"
-
-    # Travel forward 25 hours
-    travel 25.hours do
-      cached_svg = Rails.cache.read(cache_key)
-      assert_nil cached_svg, "Cache should expire after 24 hours"
+    assert_no_queries do
+      second_svg = builder.build_cached
     end
+
+    assert_equal first_svg, second_svg
   end
 
   test "#build_cached should accept custom expiration time" do
@@ -86,16 +50,16 @@ class UserHeatmapBuilderTest < ActiveSupport::TestCase
 
     Rails.cache = ActiveSupport::Cache::MemoryStore.new
 
-    builder = UserHeatmapBuilder.new(user)
-    builder.build_cached(expires_in: 1.hour)
+    ttl = 1.hour
+    builder = UserHeatmapBuilder.new(user, expires_in: ttl)
+    builder.build_cached
 
     cache_key = "user:#{user.id}:heatmap_svg:#{Date.current}"
     assert_not_nil Rails.cache.read(cache_key), "Cache should exist initially"
 
-    # Travel forward 2 hours
-    travel 2.hours do
+    travel_to (ttl + 1.second).from_now do
       cached_svg = Rails.cache.read(cache_key)
-      assert_nil cached_svg, "Cache should expire after 1 hour"
+      assert_nil cached_svg, "Cache should expire"
     end
   end
 end
