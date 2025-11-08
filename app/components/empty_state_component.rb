f2b5047b54053337ad1bf0e8cfe20130f66f974c
Alex Musayev <alex.musayev@gmail.com>
2025-11-08 20:33:18 +0100

    Adjust empty state placeholder border styling

--- app/components/empty_state_component.rb
+++ app/components/empty_state_component.rb
@@ -1,6 +1,6 @@
 class EmptyStateComponent < ViewComponent::Base
   def call
-    content_tag :div, class: "ff-card" do
+    content_tag :div, class: "ff-card border-dashed border-slate-300 shadow-none" do
       content_tag :div, class: "ff-card__body text-center py-12" do
         content
       end
