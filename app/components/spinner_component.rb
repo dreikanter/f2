ff1e7c18c21932f03fc5beb7d40cd2b4dd6bdb64
Alex Musayev <alex.musayev@gmail.com>
2025-11-09 17:15:50 +0100

    Simplify classes configuration for the spinner component

--- app/components/spinner_component.rb
+++ app/components/spinner_component.rb
@@ -1,12 +1,7 @@
 class SpinnerComponent < ViewComponent::Base
-  def initialize(size: "w-8 h-8", color: "text-gray-300", fill: "fill-sky-600", css_class: nil)
-    @size = size
-    @color = color
-    @fill = fill
-    @css_class = css_class
-  end
+  attr_reader :css_class
 
-  def classes
-    class_names(@size, @color, "animate-spin", @fill, @css_class)
+  def initialize(css_class: nil)
+    @css_class = css_class
   end
 end
--- test/components/spinner_component_test.rb
+++ test/components/spinner_component_test.rb
@@ -5,23 +5,13 @@ class SpinnerComponentTest < ViewComponent::TestCase
   test "should render SVG spinner" do
     result = render_inline(SpinnerComponent.new)
 
-    svg = result.css("svg").first
-    assert_not_nil svg
-    assert_equal "true", svg["aria-hidden"]
-    assert svg["class"].present?
+    assert result.css("svg")
   end
 
   test "should accept custom parameters" do
-    result = render_inline(
-      SpinnerComponent.new(
-        size: "w-12 h-12",
-        color: "text-blue-500",
-        fill: "fill-blue-800",
-        css_class: "mr-3"
-      )
-    )
-
+    result = render_inline(SpinnerComponent.new(css_class: "test"))
     svg = result.css("svg").first
-    assert_not_nil svg
+
+    assert_includes svg["class"], "test"
   end
 end
