5d48e451e879baa7e522578476c0cc758b8cd404
Alex Musayev <alex.musayev@gmail.com>
2025-11-09 16:34:02 +0100

    Remove unnecessary method

--- app/components/freefeed_user_component.rb
+++ app/components/freefeed_user_component.rb
@@ -2,14 +2,4 @@ class FreefeedUserComponent < ViewComponent::Base
   def initialize(username:)
     @username = username
   end
-
-  private
-
-  def avatar_classes
-    class_names(
-      "w-12 h-12",
-      "bg-slate-200",
-      "rounded-md"
-    )
-  end
 end
