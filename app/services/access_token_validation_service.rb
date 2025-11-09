153363b074feddef98797217a97f90862c4a0244
Alex Musayev <alex.musayev@gmail.com>
2025-11-09 16:41:42 +0100

    Typo

--- app/services/access_token_validation_service.rb
+++ app/services/access_token_validation_service.rb
@@ -5,7 +5,7 @@ class AccessTokenValidationService
     @access_token = access_token
   end
 
-  # TBD: Refactor this afte the required logic is clarified
+  # TBD: Refactor this after the required logic is clarified
   def call
     user_info = freefeed_client.whoami
 
