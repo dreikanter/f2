25d2e6385c9c7be7f129c5e0d5f849447668a4c7
Alex Musayev <alex.musayev@gmail.com>
2025-11-30 20:17:43 +0100

    Add instance variables for status page state logic

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,8 @@
 class StatusesController < ApplicationController
   def show
     @user = Current.user
+    @has_active_tokens = @user.access_tokens.active.any?
+    @has_feeds = @user.total_feeds_count > 0
     @recent_events = Event.where(user: @user).user_relevant.recent.limit(10)
   end
 end
