c852ec7aedfeb73e3ba1df39026fb6b52b7beaa0
Alex Musayev <alex.musayev@gmail.com>
2026-05-09 22:14:27 +0700

    Fix Rails warning noise (#353)

--- config/initializers/file_delivery.rb
+++ config/initializers/file_delivery.rb
@@ -1,3 +1,5 @@
 require_relative "../../lib/file_delivery"
 
-ActionMailer::Base.add_delivery_method :file, FileDelivery
+ActiveSupport.on_load(:action_mailer) do
+  ActionMailer::Base.add_delivery_method :file, FileDelivery
+end
