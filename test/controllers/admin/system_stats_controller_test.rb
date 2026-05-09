0fe9ac15eb95cf44d4772b03f87a96e6c782f6a5
Alex Musayev <alex.musayev@gmail.com>
2026-05-09 22:58:53 +0700

    Show deployed version stats (#355)

--- test/controllers/admin/system_stats_controller_test.rb
+++ test/controllers/admin/system_stats_controller_test.rb
@@ -36,9 +36,40 @@ class Admin::SystemStatsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should show deployed version details" do
+    with_release_env(
+      "APP_REVISION" => "0123456789abcdef",
+      "APP_REVISION_SHORT" => "0123456",
+      "APP_DEPLOYED_AT" => "2026-05-09T12:34:56Z"
+    ) do
+      login_as(admin_user)
+
+      get admin_system_stats_path
+    end
+
+    assert_response :success
+    assert_select "[data-key='release.revision.value'] code", text: "0123456"
+    assert_select "[data-key='release.deployed_at.value'] code", text: /9 May 2026, 12:34/
+    assert_select "[data-key='release.environment.value'] code", text: Rails.env
+  end
+
   private
 
   def login_as(user)
     post session_path, params: { email_address: user.email_address, password: "password123" }
+  end
+
+  def with_release_env(values)
+    previous_values = values.keys.index_with { |key| ENV.fetch(key, nil) }
+    values.each { |key, value| ENV[key] = value }
+    yield
+  ensure
+    previous_values.each do |key, value|
+      if value.nil?
+        ENV.delete(key)
+      else
+        ENV[key] = value
+      end
+    end
   end
 end
