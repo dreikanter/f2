5238c5e378d215723b4c0164c23e111922331492
Alex Musayev <alex.musayev@gmail.com>
2026-05-16 17:08:12 +0200

    Drop URL backfill from migration
    No production data exists, so the JSONB backfill is dead code. Empty-DB
    migrations stay symmetric without it.

--- db/migrate/20260516021221_drop_url_add_params_to_feeds.rb
+++ db/migrate/20260516021221_drop_url_add_params_to_feeds.rb
@@ -1,25 +1,11 @@
 class DropUrlAddParamsToFeeds < ActiveRecord::Migration[8.2]
   def up
     add_column :feeds, :params, :jsonb, null: false, default: {}
-
-    execute <<~SQL.squish
-      UPDATE feeds
-      SET params = jsonb_build_object('url', url)
-      WHERE url IS NOT NULL
-    SQL
-
     remove_column :feeds, :url
   end
 
   def down
-    add_column :feeds, :url, :string
-
-    execute <<~SQL.squish
-      UPDATE feeds
-      SET url = params ->> 'url'
-    SQL
-
-    change_column_null :feeds, :url, false
+    add_column :feeds, :url, :string, null: false
     remove_column :feeds, :params
   end
 end
