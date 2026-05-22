8ad531f30c45ac14c8ef4e21c577b02a86ef3b9b
Alex Musayev <alex.musayev@gmail.com>
2026-05-22 23:46:32 +0200

    Refactor `LlmCredentials::DefaultsController`

--- app/controllers/llm_credentials/defaults_controller.rb
+++ app/controllers/llm_credentials/defaults_controller.rb
@@ -1,15 +1,21 @@
 class LlmCredentials::DefaultsController < ApplicationController
   def update
-    credential = scope.find(params[:llm_credential_id])
     authorize credential, :update?
 
     credential.make_default!
-
-    provider_name = LlmProvider.find(credential.provider)&.display_name
-    redirect_to llm_credentials_path, notice: "'#{credential.display_name}' is now the default for #{provider_name}."
+    message = "'#{credential.display_name}' is now the default for #{provider_name}."
+    redirect_to llm_credentials_path, notice: message
   end
 
   private
+
+  def provider_name
+    @provider_name ||= LlmProvider.find(credential.provider)&.display_name
+  end
+
+  def credential
+    @credential ||= scope.find(params[:llm_credential_id])
+  end
 
   def scope
     policy_scope(LlmCredential)
