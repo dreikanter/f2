d123e94c56b8e42d8d47bdbf4321a2dfbf97be92
Alex Musayev <alex.musayev@gmail.com>
2026-05-22 23:58:12 +0200

    Use strict locals in access_tokens/show_content partial

--- app/controllers/access_tokens/validations_controller.rb
+++ app/controllers/access_tokens/validations_controller.rb
@@ -1,9 +1,13 @@
 class AccessTokens::ValidationsController < ApplicationController
   def show
-    @access_token = find_access_token
-    authorize @access_token, policy_class: AccessTokenPolicy
+    access_token = find_access_token
+    authorize access_token, policy_class: AccessTokenPolicy
 
-    render turbo_stream: turbo_stream.update("access-token-show", partial: "access_tokens/show_content")
+    render turbo_stream: turbo_stream.update(
+      "access-token-show",
+      partial: "access_tokens/show_content",
+      locals: { access_token: access_token }
+    )
   end
 
   private
