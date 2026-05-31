6dd71ce3b49d85e2dec4a16e0be28cf898a364fc
Alex Musayev <alex.musayev@gmail.com>
2026-05-31 17:10:50 +0200

    Purge expired events in batches (#519)

---
+++ app/jobs/purge_expired_events_job.rb
@@ -0,0 +1,21 @@
+# Deletes events whose explicit expiration time has passed.
+#
+# This job intentionally deletes in small batches so a daily cron run does not
+# hold locks on the events table for one long operation.
+class PurgeExpiredEventsJob < ApplicationJob
+  queue_as :default
+
+  BATCH_SIZE = 500
+  BATCH_PAUSE = 0.01.seconds
+
+  def perform
+    deleted_count = 0
+
+    Event.expired.in_batches(of: BATCH_SIZE) do |events|
+      deleted_count += events.delete_all
+      sleep BATCH_PAUSE
+    end
+
+    deleted_count
+  end
+end
