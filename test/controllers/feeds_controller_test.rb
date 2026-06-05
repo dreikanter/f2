47063cc80190d651a5d880fe3dbcc248a4483cc7
Alex Musayev <alex.musayev@gmail.com>
2026-06-05 21:54:49 +0200

    Show group link for any state, add label

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -417,24 +417,25 @@ class FeedsControllerTest < ActionDispatch::IntegrationTest
     assert_select "a[href='#{edit_feed_path(feed)}']", text: "Edit"
   end
 
-  test "#show should link to the Freefeed group for an enabled feed" do
+  test "#show should link to the Freefeed group regardless of feed state" do
     sign_in_as(user)
-    enabled = create(:feed, :enabled, user: user, access_token: access_token, target_group: "testgroup")
+    disabled = create(:feed, :disabled, user: user, access_token: access_token, target_group: "testgroup")
 
-    get feed_url(enabled)
+    get feed_url(disabled)
 
     assert_response :success
     assert_select "a[href='#{access_token.host}/testgroup']",
                   text: "#{access_token.host_domain}/testgroup"
   end
 
-  test "#show should not link to the Freefeed group for a disabled feed" do
+  test "#show should not link to the Freefeed group when target group is missing" do
     sign_in_as(user)
+    no_group = create(:feed, :without_access_token, user: user)
 
-    get feed_url(feed)
+    get feed_url(no_group)
 
     assert_response :success
-    assert_select "a[href='#{feed.access_token.host}/#{feed.target_group}']", count: 0
+    assert_select "a[href$='/testgroup']", count: 0
   end
 
   test "#show should not offer a status toggle for a draft feed" do
