462c3f598ad7db6fc8d8599fa31d36f4c90631d1
Alex Musayev <alex.musayev@gmail.com>
2026-06-11 21:36:33 +0200

    Extract dev tools to dedicated page (#684)
    * Add display names to permission types
    
    * Move dev tools to dedicated devtools page
    
    * Formatting

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -7,10 +7,24 @@ class Permission < ApplicationRecord
   AVAILABLE_PERMISSIONS = [ADMIN, DEV].freeze
 
   LABELS = {
-    ADMIN => { name: "Admin", description: "Full admin panel access and user management." },
-    DEV   => { name: "Dev", description: "Developer tools and experimental features." }
+    ADMIN => {
+      display_name: "Admin",
+      description: "Full admin panel access and user management."
+    },
+    DEV => {
+      display_name: "Developer Tools",
+      description: "Developer tools and experimental features."
+    }
   }.freeze
 
   validates :name, presence: true, inclusion: { in: AVAILABLE_PERMISSIONS }
   validates :user_id, uniqueness: { scope: :name }
+
+  def self.display_name(name)
+    LABELS.dig(name, :display_name)
+  end
+
+  def display_name
+    self.class.display_name(name)
+  end
 end
