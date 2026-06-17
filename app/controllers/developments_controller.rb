3bb76143db22ec380be044f38d9cdd30df417fdf
Alex Musayev <alex.musayev@gmail.com>
2026-06-17 14:27:55 +0200

    Disable sent emails card when email capture is not configured (#770)
    * Disable sent emails card when email capture is not configured
    
    * Also enable sent emails link when Resend is configured

--- app/controllers/developments_controller.rb
+++ app/controllers/developments_controller.rb
@@ -1,5 +1,7 @@
 class DevelopmentsController < ApplicationController
   def show
     authorize :access, :dev?
+    @sent_emails_available = ActionMailer::Base.delivery_method == :file ||
+      (ActionMailer::Base.delivery_method == :resend && Resend.api_key.present?)
   end
 end
