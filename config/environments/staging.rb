88af267c071e0156e10365008b1cac1bcb1aff6e
Alex Musayev <alex.musayev@gmail.com>
2026-06-23 18:28:56 +0200

    Enable view source annotations on staging (#844)
    * Enable view source annotations on staging
    
    Wrap rendered views and ViewComponents in source-file HTML comments on
    staging to aid debugging, while production keeps them off.
    
    * Enable view annotation in staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,1 +1,6 @@
 require_relative "production"
+
+Rails.application.configure do
+  # Annotate rendered views and ViewComponents with source-file comments
+  config.action_view.annotate_rendered_view_with_filenames = true
+end
