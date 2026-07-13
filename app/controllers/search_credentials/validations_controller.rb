08a3354fa675ba31aa83022c6869ab4b736d23da
Alex Musayev <alex.musayev@gmail.com>
2026-07-14 00:52:55 +0200

    Add search credential polling endpoint

---
+++ app/controllers/search_credentials/validations_controller.rb
@@ -0,0 +1,14 @@
+class SearchCredentials::ValidationsController < ApplicationController
+  def show
+    search_credential = policy_scope(SearchCredential).find(params[:search_credential_id])
+    authorize search_credential, :show?, policy_class: SearchCredentialPolicy
+
+    return head :no_content if search_credential.pending? || search_credential.validating?
+
+    render turbo_stream: turbo_stream.update(
+      "search-credential-show",
+      partial: "search_credentials/show_content",
+      locals: { search_credential: search_credential }
+    )
+  end
+end
