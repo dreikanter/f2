0540b6a85333ca363c25d285f10598c4433b94a4
Alex Musayev <alex.musayev@gmail.com>
2026-07-14 22:38:43 +0200

    Map compact UUIDs to application pages

--- app/helpers/identifier_helper.rb
+++ app/helpers/identifier_helper.rb
@@ -2,6 +2,15 @@ module IdentifierHelper
   UUID_SUFFIX_LENGTH = 5
   UUID_LINK_CLASSES = "font-mono underline underline-offset-2 transition hover:text-heading".freeze
 
+  EVENT_SUBJECT_MODELS = {
+    "Feed" => Feed,
+    "User" => User,
+    "Event" => Event,
+    "AccessToken" => AccessToken,
+    "AiCredential" => AiCredential,
+    "SearchCredential" => SearchCredential
+  }.freeze
+
   def short_uuid(value)
     value.to_s.last(UUID_SUFFIX_LENGTH)
   end
@@ -16,5 +25,35 @@ module IdentifierHelper
     label = uuid_label(value, prefix: prefix)
 
     path ? link_to(label, path, **html_options) : tag.span(label, **html_options)
+  end
+
+  def admin_event_subject_path(subject)
+    case subject
+    when Feed
+      admin_feed_path(subject)
+    when User
+      admin_user_path(subject)
+    when Event
+      admin_event_path(subject)
+    when AccessToken
+      access_token_path(subject)
+    when AiCredential
+      ai_credential_path(subject)
+    when SearchCredential
+      search_credential_path(subject)
+    end
+  end
+
+  def admin_event_filter_reference_path(key, value, filter:)
+    case key.to_s
+    when "user_id"
+      user = User.find_by(id: value)
+      admin_user_path(user) if user
+    when "subject_id"
+      subject_type = filter[:subject_type] || filter["subject_type"]
+      model = EVENT_SUBJECT_MODELS[subject_type]
+      subject = model&.find_by(id: value)
+      admin_event_subject_path(subject) if subject
+    end
   end
 end
