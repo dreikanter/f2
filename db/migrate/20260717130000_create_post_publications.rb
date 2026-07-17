7e5bf485fcff3c2c2b97192d6e12d3a009bb2791
Alex Musayev <alex.musayev@gmail.com>
2026-07-17 23:46:53 +0200

    Simplify post publication migration

--- db/migrate/20260717130000_create_post_publications.rb
+++ db/migrate/20260717130000_create_post_publications.rb
@@ -1,7 +1,5 @@
 class CreatePostPublications < ActiveRecord::Migration[8.2]
-  def up
-    remove_column :posts, :next_comment_index, :integer if column_exists?(:posts, :next_comment_index)
-
+  def change
     create_table :post_publications, id: :uuid, default: -> { "uuidv7()" } do |t|
       t.references :post, null: false, type: :uuid, foreign_key: { on_delete: :cascade }, index: { unique: true }
       t.integer :attachments_processed_count, default: 0, null: false
@@ -9,9 +7,5 @@ class CreatePostPublications < ActiveRecord::Migration[8.2]
       t.integer :comments_published_count, default: 0, null: false
       t.timestamps
     end
-  end
-
-  def down
-    drop_table :post_publications
   end
 end
