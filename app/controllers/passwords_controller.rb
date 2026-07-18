110222c32d132d329d61642b19c7b7f41e3d86b1
Alex Musayev <alex.musayev@gmail.com>
2026-07-19 02:02:59 +0300

    Show password validation errors

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -17,7 +17,7 @@ class PasswordsController < ApplicationController
     if @user.update(params.permit(:password, :password_confirmation))
       redirect_to new_session_path, success: "Password reset."
     else
-      redirect_to edit_password_path(params[:token]), alert: "Passwords did not match."
+      redirect_to edit_password_path(params[:token]), alert: @user.errors.full_messages.to_sentence
     end
   end
 
