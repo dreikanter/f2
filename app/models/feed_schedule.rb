615d7b3c981d1dd2b8d1f083e9348ec23fc8cd2e
Alex Musayev <alex.musayev@gmail.com>
2026-07-19 14:50:53 +0300

    Enforce one schedule per feed (#1115)
    * Enforce single feed schedule
    
    * Update schema for feed schedules uniqueness migration
    
    * Validate feed schedule uniqueness

--- app/models/feed_schedule.rb
+++ app/models/feed_schedule.rb
@@ -1,6 +1,8 @@
 class FeedSchedule < ApplicationRecord
   belongs_to :feed
 
+  validates :feed_id, uniqueness: true
+
   def calculate_next_run_at
     Fugit.parse(feed.cron_expression).next_time.to_t
   end
