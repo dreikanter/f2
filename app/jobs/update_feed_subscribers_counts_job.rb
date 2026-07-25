0c10b93745c2df211c54de8baf2b244dfe4c045b
Alex Musayev <alex.musayev@gmail.com>
2026-07-25 21:43:45 +0300

    Fix subscriber count dispatcher eligibility query

--- app/jobs/update_feed_subscribers_counts_job.rb
+++ app/jobs/update_feed_subscribers_counts_job.rb
@@ -2,7 +2,7 @@ class UpdateFeedSubscribersCountsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Feed.enabled.where.not(access_token_id: nil, target_group: nil).find_each do |feed|
+    Feed.enabled.where.not(access_token_id: nil).where.not(target_group: [nil, ""]).find_each do |feed|
       UpdateFeedSubscribersCountJob.perform_later(feed)
     end
   end
