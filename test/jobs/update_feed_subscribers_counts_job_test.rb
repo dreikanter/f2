474f3d5fbb238d2804a804b8c72439bd7a5ebd22
Alex Musayev <alex.musayev@gmail.com>
2026-07-25 17:27:34 +0300

    Fix subscriber dispatcher test setup

--- test/jobs/update_feed_subscribers_counts_job_test.rb
+++ test/jobs/update_feed_subscribers_counts_job_test.rb
@@ -3,19 +3,14 @@ require "test_helper"
 class UpdateFeedSubscribersCountsJobTest < ActiveJob::TestCase
   test "enqueues one job for each eligible feed" do
     eligible = create(:feed, :enabled)
-    disabled = create(:feed, :disabled)
-    without_token = create(:feed, :enabled, access_token: nil)
-    without_target = create(:feed, :enabled, target_group: nil)
+    create(:feed, :disabled)
+    create(:feed, :enabled, :without_access_token)
+    create(:feed, :enabled, target_group: nil)
 
     assert_enqueued_jobs 1, only: UpdateFeedSubscribersCountJob do
       UpdateFeedSubscribersCountsJob.perform_now
     end
 
     assert_enqueued_with(job: UpdateFeedSubscribersCountJob, args: [eligible])
-    assert_no_enqueued_jobs only: UpdateFeedSubscribersCountJob do
-      disabled
-      without_token
-      without_target
-    end
   end
 end
