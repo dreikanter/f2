bb3cded964b80fd48a0a8707c8cd90d15bc99898
Alex Musayev <alex.musayev@gmail.com>
2026-07-25 22:45:18 +0300

    Fix dispatcher test setup for invalid feed states

--- test/jobs/update_feed_subscribers_counts_job_test.rb
+++ test/jobs/update_feed_subscribers_counts_job_test.rb
@@ -4,8 +4,12 @@ class UpdateFeedSubscribersCountsJobTest < ActiveJob::TestCase
   test "enqueues one job for each eligible feed" do
     eligible = create(:feed, :enabled)
     create(:feed, :disabled)
-    create(:feed, :enabled, :without_access_token)
-    create(:feed, :enabled, target_group: nil)
+
+    without_access_token = create(:feed, :enabled)
+    without_access_token.update_column(:access_token_id, nil)
+
+    without_target_group = create(:feed, :enabled)
+    without_target_group.update_column(:target_group, nil)
 
     assert_enqueued_jobs 1, only: UpdateFeedSubscribersCountJob do
       UpdateFeedSubscribersCountsJob.perform_now
