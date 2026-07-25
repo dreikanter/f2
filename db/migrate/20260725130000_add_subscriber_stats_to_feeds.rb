1396611698c06edc961aa1b7ef62ae93863572a0
Alex Musayev <alex.musayev@gmail.com>
2026-07-25 18:41:06 +0300

    Fix migration compatibility version

--- db/migrate/20260725130000_add_subscriber_stats_to_feeds.rb
+++ db/migrate/20260725130000_add_subscriber_stats_to_feeds.rb
@@ -1,4 +1,4 @@
-class AddSubscriberStatsToFeeds < ActiveRecord::Migration[8.2]
+class AddSubscriberStatsToFeeds < ActiveRecord::Migration[8.0]
   def change
     add_column :feeds, :subscribers_count, :integer
     add_column :feeds, :subscribers_count_updated_at, :datetime
