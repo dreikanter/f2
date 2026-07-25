dec7f0a1c7a0afedc11e61911d3179cdcdb8c8ed
Alex Musayev <alex.musayev@gmail.com>
2026-07-25 17:25:21 +0300

    Add subscriber statistics to FreefeedClient

---
+++ lib/freefeed_client/subscriber_statistics.rb
@@ -0,0 +1,20 @@
+class FreefeedClient
+  def subscribers_count(username)
+    response = get("/v2/users/#{CGI.escapeURIComponent(username)}/statistics")
+    parse_subscribers_count_response(response.body)
+  rescue HttpClient::Error => e
+    raise Error, "Failed to fetch subscriber count: #{e.message}"
+  end
+
+  private
+
+  def parse_subscribers_count_response(body)
+    data = JSON.parse(body)
+    count = data.dig("statistics", "subscribers")
+    raise Error, "Invalid user statistics response format" if count.nil?
+
+    Integer(count)
+  rescue JSON::ParserError, ArgumentError => e
+    raise Error, "Invalid user statistics response: #{e.message}"
+  end
+end
