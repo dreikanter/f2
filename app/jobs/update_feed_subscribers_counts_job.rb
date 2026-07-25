8b957c32dc8e919e824c83241af1a0836b0443c5
Alex Musayev <alex.musayev@gmail.com>
2026-07-25 17:01:56 +0300

    Schedule subscriber count updates

---
+++ app/jobs/update_feed_subscribers_counts_job.rb
@@ -0,0 +1,9 @@
+class UpdateFeedSubscribersCountsJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    Feed.enabled.where.not(access_token_id: nil, target_group: nil).find_each do |feed|
+      UpdateFeedSubscribersCountJob.perform_later(feed)
+    end
+  end
+end
