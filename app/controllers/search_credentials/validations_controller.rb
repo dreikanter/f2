fb599cb748d96ee1d6ea3d32dee122aff8291ec9
Alex Musayev <alex.musayev@gmail.com>
2026-08-08 15:03:28 +0700

    Extract shared validation polling controller (#1226)

--- app/controllers/search_credentials/validations_controller.rb
+++ app/controllers/search_credentials/validations_controller.rb
@@ -1,14 +1,3 @@
-class SearchCredentials::ValidationsController < ApplicationController
-  def show
-    search_credential = policy_scope(SearchCredential).find(params[:search_credential_id])
-    authorize search_credential, :show?, policy_class: SearchCredentialPolicy
-
-    return head :no_content if search_credential.pending? || search_credential.validating?
-
-    render turbo_stream: turbo_stream.update(
-      "search-credential-show",
-      partial: "search_credentials/show_content",
-      locals: { search_credential: search_credential, feed_id: params[:feed_id] }
-    )
-  end
+class SearchCredentials::ValidationsController < ValidationsController
+  self.validated_class = SearchCredential
 end
