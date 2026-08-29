9316c5a1d9e1465c878f12bb99e9501d450883db
Alex Musayev <alex.musayev@gmail.com>
2026-08-29 17:26:16 +0200

    Simplify preview UUID migration

--- db/migrate/20260829170000_change_feed_preview_run_id_to_uuid.rb
+++ db/migrate/20260829170000_change_feed_preview_run_id_to_uuid.rb
@@ -1,18 +1,6 @@
 class ChangeFeedPreviewRunIdToUuid < ActiveRecord::Migration[8.0]
   def up
-    invalid_run_id = select_value(<<~SQL.squish)
-      SELECT run_id
-      FROM feed_previews
-      WHERE run_id IS NOT NULL
-        AND NOT pg_input_is_valid(run_id, 'uuid')
-      LIMIT 1
-    SQL
-
-    if invalid_run_id
-      raise ActiveRecord::MigrationError,
-            "feed_previews.run_id contains a non-UUID value; clean invalid run IDs before retrying"
-    end
-
+    execute "DELETE FROM feed_previews"
     change_column :feed_previews, :run_id, :uuid, using: "run_id::uuid"
   end
 
