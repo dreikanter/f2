3289a0f966b872cc0ff47246ef51ccef74e71656
Alex Musayev <alex.musayev@gmail.com>
2026-09-09 14:36:54 +0200

    Smooth feed identification feedback

--- test/controllers/feeds_controller_test.rb
+++ test/controllers/feeds_controller_test.rb
@@ -1362,6 +1362,8 @@ class FeedsControllerTest < ActionDispatch::IntegrationTest
     assert_select "input[data-key='form.source-edit'][value=?]", new_url
     assert_select "input[type=submit][value='Checking…'][disabled]"
     assert_select "[data-key='form.source-checking'] svg[data-icon='loader-circle'].animate-spin", count: 1
+    status = css_select("[data-key='form.source-checking'] span").sole
+    assert_equal "Checking this feed. This usually takes a few seconds.", status.text
     assert_select "[data-controller*='polling']"
     assert_includes response.body, "feed_id=#{feed.id}"
     assert_select "[data-polling-interval-value='2500'][data-polling-max-polls-value='36']"
