859ff27d4007cc39d6fa9d136736ee5198f1297b
Alex Musayev <alex.musayev@gmail.com>
2026-09-09 15:09:41 +0200

    Use sample content in Litterbox fixtures

--- test/services/profile_matcher/litterbox_profile_matcher_test.rb
+++ test/services/profile_matcher/litterbox_profile_matcher_test.rb
@@ -10,31 +10,31 @@ class ProfileMatcher::LitterboxProfileMatcherTest < ActiveSupport::TestCase
   end
 
   test "#match? should match litterboxcomics.com URLs" do
-    assert matcher("https://www.litterboxcomics.com/feed/").match?
+    assert matcher("https://www.litterboxcomics.com/sample-feed/").match?
   end
 
   test "#match? should match litterboxcomics.com without subdomain" do
-    assert matcher("https://litterboxcomics.com/feed/").match?
+    assert matcher("https://litterboxcomics.com/sample-feed/").match?
   end
 
   test "#match? should match feeds.feedburner.com URLs with litterboxcomics path" do
-    assert matcher("https://feeds.feedburner.com/litterboxcomics/yS3QAzAMEMP").match?
+    assert matcher("https://feeds.feedburner.com/litterboxcomics/sample-feed").match?
   end
 
   test "#match? should not match feedburner.com URLs without litterboxcomics path" do
-    assert_not matcher("https://feeds.feedburner.com/someotherfeed/abc123").match?
+    assert_not matcher("https://feeds.feedburner.com/sample-other-feed/sample-id").match?
   end
 
   test "#match? should not match feedburner.com URLs with litterboxcomics not at path start" do
-    assert_not matcher("https://feeds.feedburner.com/other/litterboxcomics").match?
+    assert_not matcher("https://feeds.feedburner.com/sample-other/litterboxcomics").match?
   end
 
   test "#match? should not match feedburner.com URLs with litterboxcomics as path prefix of another feed" do
-    assert_not matcher("https://feeds.feedburner.com/litterboxcomicsevil/yS3QAzAMEMP").match?
+    assert_not matcher("https://feeds.feedburner.com/litterboxcomicsevil/sample-feed").match?
   end
 
   test "#match? should not match non-feeds feedburner subdomain" do
-    assert_not matcher("https://www.feedburner.com/litterboxcomics/yS3QAzAMEMP").match?
+    assert_not matcher("https://www.feedburner.com/litterboxcomics/sample-feed").match?
   end
 
   test "#match? should not match non-litterbox URLs" do
@@ -42,7 +42,7 @@ class ProfileMatcher::LitterboxProfileMatcherTest < ActiveSupport::TestCase
   end
 
   test "#match? should not match URLs that just contain litterboxcomics in path but wrong domain" do
-    assert_not matcher("https://evillitterboxcomics.com/feed/").match?
+    assert_not matcher("https://evillitterboxcomics.com/sample-feed/").match?
   end
 
   test "#match? should handle blank inputs" do
