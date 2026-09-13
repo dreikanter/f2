bbc2ae8d83ffb61f6fc2144c68b8ca5b2cca7c65
Alex Musayev <alex.musayev@gmail.com>
2026-09-13 14:10:25 +0200

    Document provider client return values

--- app/models/llm_provider/base.rb
+++ app/models/llm_provider/base.rb
@@ -1,9 +1,12 @@
 module LlmProvider
   class Base
+    # @param api_key [String] key used to authenticate provider requests
+    # @return [Base] provider client bound to the key
     def initialize(api_key:)
       @api_key = api_key
     end
 
+    # @return [RubyLLM::Context] isolated SDK context using this client's key, with retries disabled
     def context
       RubyLLM.context do |config|
         configure(config)
@@ -11,6 +14,8 @@ module LlmProvider
       end
     end
 
+    # @abstract Implement provider model discovery.
+    # @return [Array<String>] model IDs available to this client's key
     def models
       raise NotImplementedError, "Subclasses must implement #models"
     end
