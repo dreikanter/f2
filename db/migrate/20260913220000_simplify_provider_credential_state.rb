ba4a3707476d2755b1971bfc422ba576135a0f06
Alex Musayev <alex.musayev@gmail.com>
2026-09-13 21:31:16 +0200

    Spell out credential schema changes

--- db/migrate/20260913220000_simplify_provider_credential_state.rb
+++ db/migrate/20260913220000_simplify_provider_credential_state.rb
@@ -1,10 +1,13 @@
 class SimplifyProviderCredentialState < ActiveRecord::Migration[8.2]
   def change
-    [:ai_credentials, :search_credentials].each do |table|
-      remove_index table, [:user_id, :state]
-      remove_column table, :state, :integer, default: 0, null: false
-      add_column table, :active, :boolean, default: false, null: false
-      add_index table, [:user_id, :active]
-    end
+    remove_index :ai_credentials, [:user_id, :state]
+    remove_column :ai_credentials, :state, :integer, default: 0, null: false
+    add_column :ai_credentials, :active, :boolean, default: false, null: false
+    add_index :ai_credentials, [:user_id, :active]
+
+    remove_index :search_credentials, [:user_id, :state]
+    remove_column :search_credentials, :state, :integer, default: 0, null: false
+    add_column :search_credentials, :active, :boolean, default: false, null: false
+    add_index :search_credentials, [:user_id, :active]
   end
 end
